oomek/attractplus

commit 3135623a718d6480c86f63f490db1f96738ee203

Fix disappearing artwork in the begining of playing a video cont.
Speed optimization of the previous commit.

src/fe_image.cpp
@@ -352,9 +352,10 @@ bool FeTextureContainer::load_with_ffmpeg(
 
 	m_texture.setSmooth( m_smooth );
 	m_file_name = loaded_name;
-	sf::Image img = sf::Image();
-	img.create(	m_texture.getSize().x, m_texture.getSize().y, sf::Color::Black );
-	m_texture.update( img );
+
+	for ( std::vector<FeImage *>::iterator itr = m_images.begin(); itr != m_images.end(); ++itr )
+		(*itr)->clear_sprite();
+
 	return true;
 }
 #endif
@@ -520,16 +521,12 @@ void FeTextureContainer::internal_update_selection( FeSettings *feSettings )
 				if ( try_to_load( filename, true ) )
 				{
 					loaded = true;
+					notify_texture_change();
 					break;
 				}
 			}
 		}
 	}
-
-	//
-	// Texture was replaced, so notify the attached images
-	//
-	notify_texture_change();
 }
 
 bool FeTextureContainer::tick( FeSettings *feSettings, bool play_movies )
@@ -594,6 +591,7 @@ bool FeTextureContainer::tick( FeSettings *feSettings, bool play_movies )
 
 		if ( m_movie->tick() )
 		{
+			notify_texture_change();
 			if ( m_mipmap ) m_texture.generateMipmap();
 			return true;
 		}
@@ -1054,6 +1052,16 @@ void FeImage::texture_changed( FeBaseTextureContainer *new_tex )
 	m_sprite.setTexture( m_tex->get_texture() );
 
 	//  reset texture rect now to the one reported by the new texture object
+	m_sprite.setTextureRect(
+		sf::FloatRect( 0, 0, m_tex->get_texture().getSize().x, m_tex->get_texture().getSize().y ) );
+
+	scale();
+}
+
+void FeImage::clear_sprite()
+{
+	m_sprite.setTexture( FePresent::get_blank_texture() );
+
 	m_sprite.setTextureRect(
 		sf::FloatRect( 0, 0, m_tex->get_texture().getSize().x, m_tex->get_texture().getSize().y ) );
 

src/fe_image.hpp
@@ -319,6 +319,7 @@ public:
 	bool get_visible() const;
 
 	void texture_changed( FeBaseTextureContainer *new_tex=NULL );
+	void clear_sprite();
 
 	float get_origin_x() const;
 	float get_origin_y() const;

src/fe_present.cpp
@@ -1558,6 +1558,20 @@ bool FePresent::get_mouse_pointer()
 	return false;
 }
 
+sf::Texture &FePresent::get_blank_texture()
+{
+	static sf::Texture m_blank_texture;
+
+	if ( m_blank_texture.getSize().x != 1 )
+	{
+		uint8_t arr[4]  = { 0x00,0x00,0x00,0xff };
+		m_blank_texture.create(1,1);
+		m_blank_texture.update( arr );
+	}
+
+	return m_blank_texture;
+}
+
 void FePresent::script_do_update( FeBasePresentable *bp )
 {
 	FePresent *fep = script_get_fep();

src/fe_present.hpp
@@ -269,6 +269,8 @@ public:
 	bool get_mouse_pointer();
 	void set_mouse_pointer( bool );
 
+	static sf::Texture &get_blank_texture();
+
 	//
 	// Script static functions
 	//

commit bfead2a3d1f165162b16378a1c6729e1bfa74abe

Fix missing artwork showing as black rectangles
Regression caused by commit 3135623a718d6480c86f63f490db1f96738ee203

src/fe_image.cpp
@@ -507,6 +507,7 @@ void FeTextureContainer::internal_update_selection( FeSettings *feSettings )
 		{
 			clear();
 			m_texture = sf::Texture();
+			notify_texture_change();
 		}
 		else
 		{