1d5947101ea19bd96540024597b3d2b5f168615e
Radek Dutkiewicz <radicdotkey@gmail.com>
2024-01-31 18:34:20 +0000

    Async Loader WIP
    - Rename fe.loader to fe.cache
    - Add EntryType field to input queue
    - Add cache loading functions to Squirrel

--- src/fe_vm.cpp
+++ src/fe_vm.cpp
@@ -987,6 +987,9 @@ bool FeVM::on_new_layout()
 		.Prop( _SC("queue_size"),  &FeAsyncLoader::get_queue_size )
 		.Func( _SC("cached_ref_count"),  &FeAsyncLoader::get_cached_ref_count )
 		.Func( _SC("active_ref_count"),  &FeAsyncLoader::get_active_ref_count )
+		.Func( _SC("load_image"),  &FeAsyncLoader::add_texture )
+		.Func( _SC("load_font"),  &FeAsyncLoader::add_font )
+		.Func( _SC("load_sound"),  &FeAsyncLoader::add_soundbuffer )
 	);
 
 	//
@@ -1099,7 +1102,7 @@ bool FeVM::on_new_layout()
 	fe.SetInstance( _SC("image_cache"), &il );
 
 	FeAsyncLoader &al = FeAsyncLoader::get_ref();
-	fe.SetInstance( _SC("loader"), &al );
+	fe.SetInstance( _SC("cache"), &al );
 
 	fe.SetValue( _SC("plugin"), Table() ); // an empty table for plugins to use/abuse
 
