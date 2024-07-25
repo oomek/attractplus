3f643535fe628b3f7d6d1762eed051e047261ce4
Radek Dutkiewicz <radicdotkey@gmail.com>
2024-07-25 11:45:00 +0100

    SFML 3D texCoord patch

--- extlibs/SFML/src/SFML/Graphics/Vertex.cpp
+++ extlibs/SFML/src/SFML/Graphics/Vertex.cpp
@@ -34,7 +34,7 @@ namespace sf
 Vertex::Vertex() :
 position (0, 0),
 color    (255, 255, 255),
-texCoords(0, 0)
+texCoords(0, 0, 0)
 {
 }
 
@@ -43,7 +43,7 @@ texCoords(0, 0)
 Vertex::Vertex(const Vector2f& thePosition) :
 position (thePosition),
 color    (255, 255, 255),
-texCoords(0, 0)
+texCoords(0, 0, 0)
 {
 }
 
@@ -52,13 +52,13 @@ texCoords(0, 0)
 Vertex::Vertex(const Vector2f& thePosition, const Color& theColor) :
 position (thePosition),
 color    (theColor),
-texCoords(0, 0)
+texCoords(0, 0, 0)
 {
 }
 
 
 ////////////////////////////////////////////////////////////
-Vertex::Vertex(const Vector2f& thePosition, const Vector2f& theTexCoords) :
+Vertex::Vertex(const Vector2f& thePosition, const Vector3f& theTexCoords) :
 position (thePosition),
 color    (255, 255, 255),
 texCoords(theTexCoords)
@@ -67,7 +67,7 @@ texCoords(theTexCoords)
 
 
 ////////////////////////////////////////////////////////////
-Vertex::Vertex(const Vector2f& thePosition, const Color& theColor, const Vector2f& theTexCoords) :
+Vertex::Vertex(const Vector2f& thePosition, const Color& theColor, const Vector3f& theTexCoords) :
 position (thePosition),
 color    (theColor),
 texCoords(theTexCoords)
