oomek/attractplus

commit f264e43f8ca493360a3ba66128682ae424944e60

Fix fe.Text char_size issues
- returning wrong value when the msg is empty
- returning wrong value when the layout size is overridden

src/fe_text.cpp
@@ -39,6 +39,7 @@ FeText::FeText( FePresentableParent &p, const std::string &str,
 	m_position( x, y ),
 	m_scale_factor( 1.0 )
 {
+	update_font_size();
 }
 
 void FeText::setFont( const sf::Font &f )
@@ -130,18 +131,22 @@ void FeText::on_new_list( FeSettings *s )
 	// so we do not render any unnecessary glyphs when the script updates the height of text
 	//
 	if ( m_string.size() > 0 )
-	{
-		int char_size = 8 * m_scale_factor;
-		if ( m_user_charsize > 0 )
-			char_size = m_user_charsize * m_scale_factor;
-		else if ( m_size.y > 12 )
-			char_size = ( m_size.y - 4 ) * m_scale_factor;
+		update_font_size();
 
-		m_draw_text.setTextScale( sf::Vector2f( 1.f / m_scale_factor, 1.f / m_scale_factor ) );
-		m_draw_text.setCharacterSize( char_size );
-	}
 	m_draw_text.setPosition( m_position );
 	m_draw_text.setSize( m_size );
+}
+
+void FeText::update_font_size()
+{
+	int char_size = 8 * m_scale_factor;
+	if ( m_user_charsize > 0 )
+		char_size = m_user_charsize * m_scale_factor;
+	else if ( m_size.y > 12 )
+		char_size = ( m_size.y - 4 ) * m_scale_factor;
+
+	m_draw_text.setTextScale( sf::Vector2f( 1.f / m_scale_factor, 1.f / m_scale_factor ) );
+	m_draw_text.setCharacterSize( char_size );
 }
 
 void FeText::on_new_selection( FeSettings *feSettings )
@@ -295,6 +300,9 @@ int FeText::get_bga()
 
 int FeText::get_charsize()
 {
+	if ( m_user_charsize > 0 )
+		return m_user_charsize;
+
 	return m_draw_text.getCharacterSize();
 }
 

src/fe_text.hpp
@@ -120,6 +120,8 @@ private:
 	FeText( const FeText & );
 	FeText &operator=( const FeText & );
 
+	void update_font_size();
+
 	FeTextPrimitive m_draw_text;
 	std::string m_string;
 	std::string m_string_wrapped;

commit 1e88da0046fe45b2585b1e0b297ada4885e080ff

Fix invisible text in some scenarios
regression caused by commit f264e43f8ca493360a3ba66128682ae424944e60

src/fe_text.cpp
@@ -451,6 +451,7 @@ void FeText::set_charsize(int s)
 	if ( s != m_user_charsize )
 	{
 		m_user_charsize = s;
+		update_font_size();
 		FePresent::script_do_update( this );
 	}
 }