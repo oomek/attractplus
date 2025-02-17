oomek/attractplus

commit fed1f2650fb678baebe1e0d4a8ae233003d23eec

Refactor display indices and ShowDisplaysMenu handling
- Decouple m_current_display and m_current_search_index
- Rename m_current_display to m_current_display_index
- Move ShowDisplaysMenu handling from main.cpp to fe_settings.cpp
- Don't process current display's filters when opening custom displays menu on start

src/fe_settings.cpp
@@ -24,6 +24,7 @@
 #include "fe_settings.hpp"
 #include "fe_present.hpp"
 #include "fe_cache.hpp"
+#include "fe_vm.hpp"
 #include "image_loader.hpp"
 #include "zip.hpp"
 #include <iostream>
@@ -305,6 +306,7 @@ FeSettings::FeSettings( const std::string &config_path )
 	m_joy_thresh( 75 ),
 	m_mouse_thresh( 10 ),
 	m_current_search_index( 0 ),
+	m_current_display_index( 0 ),
 	m_displays_menu_exit( true ),
 	m_hide_brackets( false ),
 	m_group_clones( false ),
@@ -421,6 +423,11 @@ void FeSettings::load()
 	FeRomListSorter::init_title_rex( rex_str );
 
 	load_state();
+
+	m_current_display_index = m_current_display;
+	if ( get_startup_mode() == ShowDisplaysMenu )
+		m_current_display = -1;
+
 	init_display();
 
 	// Make sure we have some keyboard mappings
@@ -757,7 +764,7 @@ void FeSettings::save_state()
 		display_idx = m_display_stack.front();
 
 	if ( display_idx < 0 )
-		display_idx = m_current_search_index;
+		display_idx = m_current_display_index;
 
 	m_rl.save_state();
 
@@ -991,7 +998,7 @@ int FeSettings::get_rom_index( int filter_index, int offset ) const
 	int retval, rl_size;
 	if ( m_current_display < 0 )
 	{
-		retval = m_current_search_index;
+		retval = m_current_display_index;
 		rl_size = m_rl.filter_size( filter_index );
 	}
 	else if ( !m_current_search.empty()
@@ -1478,10 +1485,10 @@ int FeSettings::display_menu_get_current_selection_as_absolute_display_index()
 {
 	ASSERT( m_current_display < 0 );
 
-	if (( m_current_search_index < 0 ) || ( m_current_search_index >= (int)m_display_menu.size() ))
+	if (( m_current_display_index < 0 ) || ( m_current_display_index >= (int)m_display_menu.size() ))
 		return -1;
 
-	return m_display_menu[ m_current_search_index ];
+	return m_display_menu[ m_current_display_index ];
 }
 
 bool FeSettings::set_display( int index, bool stack_previous )
@@ -1501,16 +1508,19 @@ bool FeSettings::set_display( int index, bool stack_previous )
 				m_display_stack.pop_back();
 			}
 			else
-				index = m_current_display;
+				index = m_current_display_index;
 		}
 		else if (( m_current_display != index ) && ( m_current_display >= 0 ))
+		{
+			m_current_display_index = m_current_display;
 			m_display_stack.push_back( m_current_display );
+		}
 
 	}
 	else
 	{
 		if ( index < 0 )
-			m_current_search_index = find_idx_in_vec( m_current_display, m_display_menu );
+			m_current_display_index = find_idx_in_vec( m_current_display, m_display_menu );
 
 		// If not stacking, clear the existing back stack (if any)
 		m_display_stack.clear();
@@ -1616,9 +1626,11 @@ void FeSettings::set_current_selection( int filter_index, int rom_index )
 {
 	// handle situation where we are currently showing a search result or clone group
 	//
-	if (( m_current_display < 0 )
-		|| ( !m_current_search.empty()
-			&& ( get_current_filter_index() == filter_index )))
+	if ( m_current_display < 0 )
+	{
+		m_current_display_index = rom_index;
+	}
+	else if (( !m_current_search.empty() && ( get_current_filter_index() == filter_index )))
 	{
 		m_current_search_index = rom_index;
 	}

src/fe_settings.hpp
@@ -222,7 +222,8 @@ private:
 	int m_clone_index; // >= 0 if a clone group is being shown, otherwise -1
 	int m_joy_thresh;		// [1..100], 100=least sensitive
 	int m_mouse_thresh;	// [1..100], 100=least sensitive
-	int m_current_search_index; // used when custom searching *AND* when showing 'Displays Menu' w/ custom layout
+	int m_current_search_index; // used when custom searching
+	int m_current_display_index; // used as an index of currently selected display in displays menu
 	bool m_displays_menu_exit;
 	bool m_hide_brackets;
 	bool m_group_clones;
@@ -595,6 +596,7 @@ public:
 		std::vector<int> &indices,
 		int &current_idx ) const;
 
+	// Used only when Edit Game is pressed in Displays Menu
 	int display_menu_get_current_selection_as_absolute_display_index();
 
 	FeDisplayInfo *get_display( int index );

src/main.cpp
@@ -201,8 +201,6 @@ int main(int argc, char *argv[])
 				break;
 
 			case FeSettings::ShowDisplaysMenu:
-				feSettings.set_display(-1);
-				FeVM::cb_signal( "displays_menu" );
 				break;
 
 			default:
@@ -552,26 +550,18 @@ int main(int argc, char *argv[])
 				move_triggered = FeInputMap::LAST_COMMAND;
 				move_last_triggered = 0;
 
+				feVM.load_layout( true );
 
 				switch ( feSettings.get_startup_mode() )
 				{
 				case FeSettings::LaunchLastGame:
-					feVM.load_layout( true );
 					feSettings.select_last_launch();
 					launch_game=true;
 					break;
 
 				case FeSettings::ShowDisplaysMenu:
-					if ( feSettings.get_current_display_index() != -1 )
-						feSettings.set_display(-1);
-
-					feVM.load_layout( true );
 					if ( feSettings.get_info( FeSettings::MenuLayout ).empty() )
 						FeVM::cb_signal( "displays_menu" );
-					break;
-
-				default:
-					feVM.load_layout( true );
 					break;
 				}
 

commit ce70793f565e7460c8e5be79f1b72dc31d3f8df3

Displays Menu fixes
- Fix current display index reset in default Displays Menu after restarting AM
- Fix 2 default Displays Menus showing one on top of another. Regression caused by fed1f2650fb678baebe1e0d4a8ae233003d23eec
- Fix Displays Menu not showing when exiting the intro

src/fe_settings.cpp
@@ -307,6 +307,7 @@ FeSettings::FeSettings( const std::string &config_path )
 	m_mouse_thresh( 10 ),
 	m_current_search_index( 0 ),
 	m_current_display_index( 0 ),
+	m_actual_display_index( 0 ),
 	m_displays_menu_exit( true ),
 	m_hide_brackets( false ),
 	m_group_clones( false ),
@@ -764,7 +765,7 @@ void FeSettings::save_state()
 		display_idx = m_display_stack.front();
 
 	if ( display_idx < 0 )
-		display_idx = m_current_display_index;
+		display_idx = m_actual_display_index;
 
 	m_rl.save_state();
 
@@ -815,6 +816,7 @@ void FeSettings::load_state()
 		token_helper( line, pos, tok, ";" );
 
 		m_current_display = as_int( tok );
+		m_actual_display_index = m_current_display;
 
 		token_helper( line, pos, tok, ";" );
 
@@ -1528,6 +1530,9 @@ bool FeSettings::set_display( int index, bool stack_previous )
 
 	m_current_display = index;
 
+	if ( m_current_display != -1 )
+		m_actual_display_index = m_current_display;
+
 	m_rl.save_state();
 	init_display();
 
@@ -1609,6 +1614,11 @@ bool FeSettings::navigate_filter( int step )
 int FeSettings::get_current_display_index() const
 {
 	return m_current_display;
+}
+
+int FeSettings::get_actual_display_index() const
+{
+	return m_actual_display_index;
 }
 
 int FeSettings::get_display_index_from_name( const std::string &n ) const

src/fe_settings.hpp
@@ -224,6 +224,7 @@ private:
 	int m_mouse_thresh;	// [1..100], 100=least sensitive
 	int m_current_search_index; // used when custom searching
 	int m_current_display_index; // used as an index of currently selected display in displays menu
+	int m_actual_display_index; // The most recent display index that was selected in the displays menu
 	bool m_displays_menu_exit;
 	bool m_hide_brackets;
 	bool m_group_clones;
@@ -367,6 +368,7 @@ public:
 	bool back_displays_available() { return !m_display_stack.empty(); };
 
 	int get_current_display_index() const;
+	int get_actual_display_index() const;
 	int get_display_index_from_name( const std::string &name ) const;
 	int displays_count() const;
 

src/main.cpp
@@ -201,6 +201,12 @@ int main(int argc, char *argv[])
 				break;
 
 			case FeSettings::ShowDisplaysMenu:
+				if ( feSettings.get_info( FeSettings::MenuLayout ).empty() )
+				{
+					feSettings.set_display( feSettings.get_actual_display_index() );
+					feVM.load_layout( true );
+					FeVM::cb_signal( "displays_menu" );
+				}
 				break;
 
 			default:
@@ -241,6 +247,9 @@ int main(int argc, char *argv[])
 					window.initial_create();
 					feVM.init_monitors();
 				}
+
+				if ( feSettings.get_info( FeSettings::MenuLayout ).empty() )
+					feSettings.set_display( feSettings.get_actual_display_index() );
 
 				feVM.load_layout();
 			}
@@ -549,6 +558,9 @@ int main(int argc, char *argv[])
 				move_state = FeInputMap::LAST_COMMAND;
 				move_triggered = FeInputMap::LAST_COMMAND;
 				move_last_triggered = 0;
+
+				if ( feSettings.get_info( FeSettings::MenuLayout ).empty() )
+					feSettings.set_display( feSettings.get_actual_display_index() );
 
 				feVM.load_layout( true );
 