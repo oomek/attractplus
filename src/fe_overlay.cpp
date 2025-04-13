oomek/attractplus

commit 768e8b7c90439cd67ab6fa86532a401ce4ec0ca8

UI Update

src/fe_overlay.cpp
@@ -27,9 +27,13 @@
 #include "fe_listbox.hpp"
 #include "fe_text.hpp"
 #include <SFML/Graphics.hpp>
+#include "base64.hpp"
+
 #include <iostream>
 #define _USE_MATH_DEFINES
 #include <cmath>
+
+#include "Logo.png.h"
 
 class FeConfigContextImp : public FeConfigContext
 {
@@ -229,6 +233,13 @@ void FeOverlay::init()
 	m_fade_alpha = 80;
 	m_line_size = std::round( std::min( m_screen_size.x, m_screen_size.y ) / 240 );
 	m_font = m_fePresent.get_default_font();
+
+	std::vector<unsigned char> logo_data = base64_decode( _binary_resources_images_Logo_png );
+	if ( m_logo.loadFromMemory( logo_data.data(), logo_data.size() ))
+	{
+		m_logo.setSmooth( true );
+		m_logo.generateMipmap();
+	}
 
 	style_init();
 }
@@ -1061,11 +1072,21 @@ int FeOverlay::display_config_dialog(
 	FeTextPrimitive header( m_font, m_header_text_colour, m_edge_bg_color, m_header_size );
 
 	header.setSize( m_screen_size.x, m_edge_size );
+	header.setAlignment( static_cast<FeTextPrimitive::Alignment>( FeTextPrimitive::Left | FeTextPrimitive::Middle ));
+	header.setPosition( 0, 0 );
+	header.setMargin( m_edge_size );
 	header.setBgOutlineColor( m_edge_line_colour );
 	header.setBgOutlineThickness( m_line_size );
 	header.setTextScale( m_text_scale );
 	header.setString( ctx.title );
 	draw_list.push_back( &header );
+
+	sf::Sprite logo( m_logo );
+	draw_list.push_back( &logo );
+	float scale_factor = ( 32.0f / 256.0f ) * ( m_screen_size.y / 480.0f );
+	logo.setScale({ scale_factor, scale_factor });
+	float offset = std::floor( m_edge_size * ( 14.0f / 60.0f ));
+	logo.setPosition({ offset, offset });
 
 	unsigned int width = m_screen_size.x;
 	if ( ctx.style == FeConfigContext::EditList )

src/fe_overlay.hpp
@@ -62,6 +62,7 @@ private:
 	int m_fade_alpha;
 	const sf::Font *m_font;
 	FeInputMap::Command m_menu_command;
+	sf::Texture m_logo;
 
 	FeOverlay( const FeOverlay & );
 	FeOverlay &operator=( const FeOverlay & );

config/language/cn.msg
@@ -31,15 +31,15 @@ Command;命令
 Command Arguments;命令及参数（ Arguments）
 Comparison;比较
 Configure;设置
-Configure / Controls;设置 / 控制器
-Configure / Displays;设置 / 界面
-Configure / Emulators;设置 / 模拟器
-Configure / General;设置 / 其它
-Configure / Intro;设置 / 开机画面
-Configure / Plug-ins;设置 / 插件
-Configure / Scraper;设置 / 搜刮
-Configure / Screen Saver;设置 / 屏保
-Configure / Sound;设置 / 声音
+Configure > Controls;设置 > 控制器
+Configure > Displays;设置 > 界面
+Configure > Emulators;设置 > 模拟器
+Configure > General;设置 > 其它
+Configure > Intro;设置 > 开机画面
+Configure > Plug-ins;设置 > 插件
+Configure > Scraper;设置 > 搜刮
+Configure > Screen Saver;设置 > 屏保
+Configure > Sound;设置 > 声音
 Configure Plug-in;设置插件
 Confirm Favourites;设置收藏
 Control;控制器(Control)
@@ -125,7 +125,7 @@ List Limit;列表大小限制
 Manufacturer;制造商
 Menu Colour;菜单颜色
 Menu Prompt;标题
-Menu Style / Layout;主题
+Menu Style > Layout;主题
 Mouse Threshold;鼠标的阈值
 Movie Volume;视频音量
 Name;名称
@@ -226,7 +226,6 @@ equals;等于
 Add a rule to this filter;添加分类规则
 Ambient Sounds;背景音乐
 Basic Movie;全屏式动态画面
-Configure / Input;设置/控制器
 Configure available emulators;可用模拟器
 Configure filter rule;设置分类规则
 Delete this filter;删除分类
@@ -256,8 +255,8 @@ Auto-detect emulators?;Auto-detect emulators?
 Blank Game;Blank Game
 Buttons;Buttons
 Clear Hotkey?;Clear Hotkey?
-Configure / Displays Menu;Configure / Displays Menu
-Configure / Joystick Mapping;Configure / Joystick Mapping
+Configure > Displays Menu;Configure > Displays Menu
+Configure > Joystick Mapping;Configure > Joystick Mapping
 Configure Layout;Configure Layout
 Confirm Exit;Confirm Exit
 Create new tag;Create new tag

config/language/de.msg
@@ -29,15 +29,15 @@ Command;Kommando
 Command Arguments;Kommandozeilenargumente
 Comparison;Vergleich
 Configure;Konfiguration
-Configure / Controls;Konfiguration / Steuerungen
-Configure / Displays;Konfiguration / Bildschirme
-Configure / Emulators;Konfiguration / Emulatoren
-Configure / General;Konfiguration / Allgemein
-Configure / Intro;Konfiguration / Intro Sequenz
-Configure / Plug-ins;Konfiguration / Plug-ins
-Configure / Scraper;Konfiguration / Scraper
-Configure / Screen Saver;Konfiguration / Bildschirmschoner
-Configure / Sound;Konfiguration / Klänge
+Configure > Controls;Konfiguration > Steuerungen
+Configure > Displays;Konfiguration > Bildschirme
+Configure > Emulators;Konfiguration > Emulatoren
+Configure > General;Konfiguration > Allgemein
+Configure > Intro;Konfiguration > Intro Sequenz
+Configure > Plug-ins;Konfiguration > Plug-ins
+Configure > Scraper;Konfiguration > Scraper
+Configure > Screen Saver;Konfiguration > Bildschirmschoner
+Configure > Sound;Konfiguration > Klänge
 Configure Plug-in;Konfiguriere Plug-in
 Confirm Favourites;Favoriten bestätigen
 Control;Steuerung
@@ -335,8 +335,8 @@ Attract-Mode detected emulator(s) that can be imported automatically.  Import th
 Blank Game;Spiele-Platzhalter
 Buttons;Tasten
 Clear Hotkey?;Tastenkombination entfernen?
-Configure / Displays Menu;Konfiguration / Bildschirmmenü
-Configure / Joystick Mapping;Konfiguration / Joystick Belegung
+Configure > Displays Menu;Konfiguration > Bildschirmmenü
+Configure > Joystick Mapping;Konfiguration > Joystick Belegung
 Configure Layout;Konfiguriere das Thema
 Confirm Exit;Beenden bestätigen
 Create new tag;neues Etikett erstellen
@@ -371,7 +371,7 @@ Launch Last Game;zuletzt gespieltes Spiel starten
 Layout File;Themendatei
 Menu Colour;Menüfarbe
 Menu Prompt;Eingabeaufforderung
-Menu Style / Layout;Menüstil / Thema
+Menu Style > Layout;Menüstil > Thema
 Next Game;nächstes Spiel
 Non-Blocking Mode Wait;Klippo-Modus Wartezeit
 Overview;Übersicht

config/language/es.msg
@@ -29,15 +29,15 @@ Command;Comandos
 Command Arguments;Argumentos
 Comparison;Comparación
 Configure;Configuración
-Configure / Controls;Configuración / Controles
-Configure / Displays;Configuración / Pantallas
-Configure / Emulators;Configuración / Emuladores
-Configure / General;Configuración / General
-Configure / Intro;Configuración / Intro
-Configure / Plug-ins;Configuración / Plugins
-Configure / Scraper;Configuración / Scraper
-Configure / Screen Saver;Configuración / Protector de pantalla
-Configure / Sound;Configuración / Sonido
+Configure > Controls;Configuración > Controles
+Configure > Displays;Configuración > Pantallas
+Configure > Emulators;Configuración > Emuladores
+Configure > General;Configuración > General
+Configure > Intro;Configuración > Intro
+Configure > Plug-ins;Configuración > Plugins
+Configure > Scraper;Configuración > Scraper
+Configure > Screen Saver;Configuración > Protector de pantalla
+Configure > Sound;Configuración > Sonido
 Configure Plug-in;Configuración de Plugins
 Confirm Favourites;Confirmar favoritos
 Control;Controles
@@ -345,8 +345,8 @@ Auto-detect emulators?;Auto-detect emulators?
 Blank Game;Blank Game
 Buttons;Buttons
 Clear Hotkey?;Clear Hotkey?
-Configure / Displays Menu;Configure / Displays Menu
-Configure / Joystick Mapping;Configure / Joystick Mapping
+Configure > Displays Menu;Configure > Displays Menu
+Configure > Joystick Mapping;Configure > Joystick Mapping
 Configure Layout;Configure Layout
 Confirm Exit;Confirm Exit
 Create new tag;Create new tag
@@ -382,7 +382,7 @@ Launch Last Game;Launch Last Game
 Layout File;Layout File
 Menu Colour;Color del menú
 Menu Prompt;Menu Prompt
-Menu Style / Layout;Menu Style / Layout
+Menu Style > Layout;Menu Style > Layout
 Next Game;Next Game
 Non-Blocking Mode Wait;Non-Blocking Mode Wait
 Overview;Overview

config/language/fr.msg
@@ -28,15 +28,15 @@ Command;Commandes
 Command Arguments;Ligne de commande
 Comparison;Comparaison
 Configure;Configuration
-Configure / Controls;Configuration / Contrôles
-Configure / Displays;Configuration / Listes
-Configure / Emulators;Configuration / Emulateurs
-Configure / General;Configuration / Général
-Configure / Intro;Configuration / Intro
-Configure / Plug-ins;Configuration / Plug-in
-Configure / Scraper;Configuration / Aspiration
-Configure / Sound;Configuration / Son
-Configure / Screen Saver;Configuration / Économiseur d'écran
+Configure > Controls;Configuration > Contrôles
+Configure > Displays;Configuration > Listes
+Configure > Emulators;Configuration > Emulateurs
+Configure > General;Configuration > Général
+Configure > Intro;Configuration > Intro
+Configure > Plug-ins;Configuration > Plug-in
+Configure > Scraper;Configuration > Aspiration
+Configure > Sound;Configuration > Son
+Configure > Screen Saver;Configuration > Économiseur d'écran
 Configure Plug-in;Configurer Plug-in
 Confirm Favourites;Confirmer les favoris
 Control;Contrôle
@@ -336,8 +336,8 @@ Auto-detect emulators?;Auto-detect emulators?
 Blank Game;Blank Game
 Buttons;Buttons
 Clear Hotkey?;Clear Hotkey?
-Configure / Displays Menu;Configure / Displays Menu
-Configure / Joystick Mapping;Configure / Joystick Mapping
+Configure > Displays Menu;Configure > Displays Menu
+Configure > Joystick Mapping;Configure > Joystick Mapping
 Configure Layout;Configure Layout
 Confirm Exit;Confirm Exit
 Create new tag;Create new tag
@@ -373,7 +373,7 @@ Launch Last Game;Launch Last Game
 Layout File;Layout File
 Menu Colour;Couleur des menus
 Menu Prompt;Menu Prompt
-Menu Style / Layout;Menu Style / Layout
+Menu Style > Layout;Menu Style > Layout
 Next Game;Next Game
 Non-Blocking Mode Wait;Non-Blocking Mode Wait
 Overview;Overview

config/language/it.msg
@@ -34,16 +34,16 @@ Command;Comando
 Command Arguments;Argomenti del comando
 Comparison;Comparazione
 Configure;Configurazione
-Configure / Controls;Configurazione / Controlli
-Configure / Displays;Configurazione / Display
-Configure / Displays Menu;Configura / Menù dei display
-Configure / Emulators;Configurazione / Emulatori
-Configure / General;Configurazione / Varie
-Configure / Intro;Configura / Video di introduzione
-Configure / Plug-ins;Configurazione / Plugin
-Configure / Scraper;Configurazione / Recupera dalla rete
-Configure / Screen Saver;Configura / Salvaschermo
-Configure / Sound;Configurazione / Suoni
+Configure > Controls;Configurazione > Controlli
+Configure > Displays;Configurazione > Display
+Configure > Displays Menu;Configura > Menù dei display
+Configure > Emulators;Configurazione > Emulatori
+Configure > General;Configurazione > Varie
+Configure > Intro;Configura > Video di introduzione
+Configure > Plug-ins;Configurazione > Plugin
+Configure > Scraper;Configurazione > Recupera dalla rete
+Configure > Screen Saver;Configura > Salvaschermo
+Configure > Sound;Configurazione > Suoni
 Configure Layout;Configura il tema
 Configure Plug-in;Configura i plugin
 Confirm Exit;Conferma l'uscita
@@ -140,7 +140,7 @@ List Limit;Límite della lista
 Manufacturer;Produttore
 Menu Colour;Colore del menù
 Menu Prompt;Titolo del menù
-Menu Style / Layout;Tema del menù
+Menu Style > Layout;Tema del menù
 Mouse Threshold;Soglia del mouse
 Movie Volume;Volume dei video
 Name;Nome
@@ -427,7 +427,7 @@ Attract-Mode detected emulator(s) that can be imported automatically.  Import th
 Auto-detect emulators?;Auto-detect emulators?
 Blank Game;Blank Game
 Clear Hotkey?;Clear Hotkey?
-Configure / Joystick Mapping;Configure / Joystick Mapping
+Configure > Joystick Mapping;Configure > Joystick Mapping
 Delete shortcut '$1'?;Delete shortcut '$1'?
 Delete this Shortcut;Delete this Shortcut
 Edit Shortcut;Edit Shortcut

config/language/jp.msg
@@ -28,15 +28,15 @@ Command;コマンド
 Command Arguments;実行パラメータ
 Comparison;ルールの比較方式
 Configure;設定
-Configure / Controls;設定 / 入力
-Configure / Displays;設定 / ディスプレイ
-Configure / Emulators;設定 / エミュレータ
-Configure / General;設定 / その他
-Configure / Intro;設定 / イントロ
-Configure / Plug-ins;設定 / プラグイン
-Configure / Scraper;設定 / オンラインゲーム情報
-Configure / Screen Saver;設定 / スクリーンセーバー
-Configure / Sound;設定 / サウンド
+Configure > Controls;設定 > 入力
+Configure > Displays;設定 > ディスプレイ
+Configure > Emulators;設定 > エミュレータ
+Configure > General;設定 > その他
+Configure > Intro;設定 > イントロ
+Configure > Plug-ins;設定 > プラグイン
+Configure > Scraper;設定 > オンラインゲーム情報
+Configure > Screen Saver;設定 > スクリーンセーバー
+Configure > Sound;設定 > サウンド
 Configure Plug-in;プラグインを設定
 Confirm Favourites;お気に入りの確認
 Control;コントローラ(Control)
@@ -339,8 +339,8 @@ Auto-detect emulators?;Auto-detect emulators?
 Blank Game;Blank Game
 Buttons;Buttons
 Clear Hotkey?;Clear Hotkey?
-Configure / Displays Menu;Configure / Displays Menu
-Configure / Joystick Mapping;Configure / Joystick Mapping
+Configure > Displays Menu;Configure > Displays Menu
+Configure > Joystick Mapping;Configure > Joystick Mapping
 Configure Layout;Configure Layout
 Confirm Exit;Confirm Exit
 Create new tag;Create new tag
@@ -373,7 +373,7 @@ Launch Last Game;Launch Last Game
 Layout File;Layout File
 Menu Colour;メニューの色
 Menu Prompt;Menu Prompt
-Menu Style / Layout;Menu Style / Layout
+Menu Style > Layout;Menu Style > Layout
 Next Game;Next Game
 Next Page;Next Page
 Non-Blocking Mode Wait;Non-Blocking Mode Wait

commit 9497572a645f7191c25ba811bb8238299adcf730

Move AM+ logo from FeOverlay to FeWindow
Fixes regression of logo not being freed in DRM.
Caused by 768e8b7c90439cd67ab6fa86532a401ce4ec0ca8

src/fe_overlay.cpp
@@ -33,8 +33,6 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 
-#include "Logo.png.h"
-
 class FeConfigContextImp : public FeConfigContext
 {
 private:
@@ -233,13 +231,6 @@ void FeOverlay::init()
 	m_fade_alpha = 80;
 	m_line_size = std::round( std::min( m_screen_size.x, m_screen_size.y ) / 240 );
 	m_font = m_fePresent.get_default_font();
-
-	std::vector<unsigned char> logo_data = base64_decode( _binary_resources_images_Logo_png );
-	if ( m_logo.loadFromMemory( logo_data.data(), logo_data.size() ))
-	{
-		m_logo.setSmooth( true );
-		m_logo.generateMipmap();
-	}
 
 	style_init();
 }
@@ -1081,7 +1072,7 @@ int FeOverlay::display_config_dialog(
 	header.setString( ctx.title );
 	draw_list.push_back( &header );
 
-	sf::Sprite logo( m_logo );
+	sf::Sprite logo( m_wnd.get_logo() );
 	draw_list.push_back( &logo );
 	float scale_factor = ( 32.0f / 256.0f ) * ( m_screen_size.y / 480.0f );
 	logo.setScale({ scale_factor, scale_factor });

src/fe_overlay.hpp
@@ -62,7 +62,6 @@ private:
 	int m_fade_alpha;
 	const sf::Font *m_font;
 	FeInputMap::Command m_menu_command;
-	sf::Texture m_logo;
 
 	FeOverlay( const FeOverlay & );
 	FeOverlay &operator=( const FeOverlay & );

src/fe_window.cpp
@@ -27,6 +27,8 @@
 #include "fe_settings.hpp"
 #include "fe_window.hpp"
 #include "fe_present.hpp"
+#include "base64.hpp"
+#include "Logo.png.h"
 
 #ifdef SFML_SYSTEM_WINDOWS
 #define WIN32_LEAN_AND_MEAN
@@ -416,6 +418,14 @@ void FeWindow::initial_create()
 	// Only mess with the mouse position if mouse moves mapped
 	if ( m_fes.test_mouse_reset( 0, 0 ) )
 		sf::Mouse::setPosition( sf::Vector2i( wsize.x / 2, wsize.y / 2 ), *m_window );
+
+	m_logo = new sf::Texture();
+	std::vector<unsigned char> logo_data = base64_decode( _binary_resources_images_Logo_png );
+	if ( m_logo->loadFromMemory( logo_data.data(), logo_data.size() ))
+	{
+		m_logo->setSmooth( true );
+		m_logo->generateMipmap();
+	}
 }
 
 void launch_callback( void *o )
@@ -707,6 +717,9 @@ sf::RenderWindow &FeWindow::get_win()
 
 void FeWindow::close()
 {
+	if ( m_logo )
+		delete m_logo;
+
 	if ( m_window )
 		m_window->display(); // Crashing on Linux workaround
 		m_window->close();

src/fe_window.hpp
@@ -23,7 +23,7 @@
 #ifndef FE_WINDOW_HPP
 #define FE_WINDOW_HPP
 
-#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics.hpp>
 
 class FeSettings;
 
@@ -43,6 +43,7 @@ private:
 	sf::RenderWindow m_blackout;
 #endif
 	int m_win_mode;
+	sf::Texture *m_logo;
 
 public:
 	FeWindow( FeSettings &fes );
@@ -65,6 +66,7 @@ public:
 	const std::optional<sf::Event> pollEvent();
 
 	sf::RenderWindow &get_win();
+	const sf::Texture &get_logo() const { return *m_logo; };
 };
 
 #endif