oomek/attractplus

commit a1bada2b9fbd602dd090b4af162b0001613a95d2

Add VU and FFT visualisers to FeMusic and FeImage
obj.vu
obj.vu_left
obj.vu_right
obj.fft[32]
obj.fft_left[32]
obj.fft_right[32]

- Add FeAudioEffect base class
- Add DC Offset FX
- Add Normaliser FX
- Fix Blackman window level to match VU and FFT amplitudes
- Fix Normaliser on media volume changes
- Increase FFT size for less spectral leakage
- Set FFT amplitudes to full logarithmic
Resampler fixes
- Fix resamples that are not a multiple of 48kHz
- Use phase accumulator resampling

src/fe_music.cpp
@@ -20,6 +20,8 @@
  *
  */
 
+#define _USE_MATH_DEFINES
+
 #include "fe_music.hpp"
 #include "fe_settings.hpp"
 #include "fe_present.hpp"
@@ -28,16 +30,69 @@
 
 #include <fstream>
 #include <unordered_map>
+#include <cmath>
+#include <memory>
+
+// Undefine Windows GetObject macro that conflict with Sqrat
+#ifdef GetObject
+#undef GetObject
+#endif
+
 
 FeMusic::FeMusic( bool loop )
 	: m_file_name( "" ),
-	m_volume( 100.0 )
+	m_volume( 100.0 ),
+	m_audio_effects()
 {
 	m_music.setLooping( loop );
+	m_audio_effects.add_effect( std::make_unique<FeAudioDCFilter>() );
+	m_audio_effects.add_effect( std::make_unique<FeAudioNormaliser>() );
+	m_audio_effects.add_effect( std::make_unique<FeAudioVisualiser>() );
 
 	FePresent *fep = FePresent::script_get_fep();
 	if ( fep )
-		m_music.setVolume( fep->get_fes()->get_play_volume( FeSoundInfo::Sound ));
+	{
+		float volume = fep->get_fes()->get_play_volume( FeSoundInfo::Sound );
+		m_music.setVolume( volume );
+
+		auto* normaliser = m_audio_effects.get_effect<FeAudioNormaliser>();
+		if ( normaliser )
+			normaliser->set_media_volume( volume / 100.0f );
+	}
+	m_music.setEffectProcessor( [this]( const float *input_frames, unsigned int &input_frame_count,
+	                                    float *output_frames, unsigned int &output_frame_count,
+	                                    unsigned int frame_channel_count )
+	{
+		FePresent *fep = FePresent::script_get_fep();
+		if ( fep )
+		{
+			auto* normaliser = m_audio_effects.get_effect<FeAudioNormaliser>();
+			if ( normaliser )
+			{
+				normaliser->set_enabled( fep->get_fes()->get_loudness() );
+			}
+		}
+
+		if ( input_frames && input_frame_count > 0 && get_playing() )
+		{
+			m_audio_effects.process_all( input_frames, output_frames,
+			                                       input_frame_count, frame_channel_count,
+			                                       static_cast<float>( m_music.getSampleRate() ));
+		}
+		else
+		{
+			m_audio_effects.reset_all();
+
+			// Copy input to output when not playing
+			if ( input_frames && output_frames && input_frame_count > 0 )
+			{
+				const unsigned int total_samples = input_frame_count * frame_channel_count;
+				std::memcpy( output_frames, input_frames, total_samples * sizeof(float) );
+			}
+		}
+
+		output_frame_count = input_frame_count;
+	});
 }
 
 FeMusic::~FeMusic()
@@ -96,6 +151,10 @@ void FeMusic::set_volume( float v )
 			v = v * fep->get_fes()->get_play_volume( FeSoundInfo::Sound ) / 100.0;
 
 		m_music.setVolume( v );
+
+		auto* normaliser = m_audio_effects.get_effect<FeAudioNormaliser>();
+		if ( normaliser )
+			normaliser->set_media_volume( v / 100.0f );
 	}
 }
 
@@ -205,4 +264,44 @@ const char *FeMusic::get_metadata( const char* tag )
 	std::unordered_map< std::string, std::string >::iterator it = metadata.find( tag );
 
 	return it != metadata.end() ? it->second.c_str() : "";
+}
+
+void FeMusic::tick()
+{
+	m_audio_effects.update_all();
+}
+
+FeAudioVisualiser* FeMusic::get_audio_visualiser()
+{
+	return m_audio_effects.get_effect<FeAudioVisualiser>();
+}
+
+float FeMusic::get_vu_mono()
+{
+	return get_audio_visualiser()->get_vu_mono();
+}
+
+float FeMusic::get_vu_left()
+{
+	return get_audio_visualiser()->get_vu_left();
+}
+
+float FeMusic::get_vu_right()
+{
+	return get_audio_visualiser()->get_vu_right();
+}
+
+Sqrat::Array FeMusic::get_fft_array_mono()
+{
+	return get_audio_visualiser()->get_fft_array_mono();
+}
+
+Sqrat::Array FeMusic::get_fft_array_left()
+{
+	return get_audio_visualiser()->get_fft_array_left();
+}
+
+Sqrat::Array FeMusic::get_fft_array_right()
+{
+	return get_audio_visualiser()->get_fft_array_right();
 }

src/media.cpp
@@ -26,6 +26,7 @@
 #include "fe_file.hpp"
 #include <SFML/Graphics.hpp>
 #include "fe_present.hpp"
+#include "fe_audio_fx.hpp"
 
 extern "C"
 {
@@ -48,6 +49,7 @@ extern "C"
 #include <iostream>
 #include <thread>
 #include <algorithm>
+#include <memory>
 
 #if (LIBAVFORMAT_VERSION_INT >= AV_VERSION_INT( 59, 0, 100 ))
 typedef const AVCodec FeAVCodec;
@@ -851,6 +853,9 @@ FeMedia::FeMedia( Type t )
 	m_aspect_ratio( 1.0 )
 {
 	m_imp = new FeMediaImp( t );
+	m_audio_effects.add_effect( std::make_unique<FeAudioDCFilter>() );
+	m_audio_effects.add_effect( std::make_unique<FeAudioNormaliser>() );
+	m_audio_effects.add_effect( std::make_unique<FeAudioVisualiser>() );
 }
 
 FeMedia::~FeMedia()
@@ -858,6 +863,44 @@ FeMedia::~FeMedia()
 	close();
 
 	delete m_imp;
+}
+
+void FeMedia::setup_effect_processor()
+{
+	setEffectProcessor( [this]( const float *input_frames, unsigned int &input_frame_count,
+	                            float *output_frames, unsigned int &output_frame_count,
+	                            unsigned int frame_channel_count )
+	{
+		FePresent *fep = FePresent::script_get_fep();
+		if ( fep )
+		{
+			auto* normaliser = m_audio_effects.get_effect<FeAudioNormaliser>();
+			if ( normaliser )
+			{
+				normaliser->set_enabled( fep->get_fes()->get_loudness() );
+			}
+		}
+
+		if ( input_frames && input_frame_count > 0 && is_playing() )
+		{
+			m_audio_effects.process_all( input_frames, output_frames,
+			                                       input_frame_count, frame_channel_count,
+			                                       static_cast<float>( getSampleRate() ));
+		}
+		else
+		{
+			m_audio_effects.reset_all();
+
+			// Copy input to output when not playing
+			if ( input_frames && output_frames && input_frame_count > 0 )
+			{
+				const unsigned int total_samples = input_frame_count * frame_channel_count;
+				std::memcpy( output_frames, input_frames, total_samples * sizeof( float ));
+			}
+		}
+
+		output_frame_count = input_frame_count;
+	});
 }
 
 sf::Time FeMedia::get_video_time()
@@ -876,6 +919,8 @@ void FeMedia::play()
 {
 	if ( !is_playing() )
 	{
+		m_audio_effects.reset_all();
+
 		if ( m_video )
 			m_video->play();
 
@@ -891,6 +936,8 @@ void FeMedia::signal_stop()
 
 	if ( m_video )
 		m_video->signal_stop();
+
+	m_audio_effects.reset_all();
 }
 
 void FeMedia::stop()
@@ -917,6 +964,8 @@ void FeMedia::stop()
 	}
 
 	m_imp->m_read_eof = false;
+
+	m_audio_effects.reset_all();
 }
 
 void FeMedia::close()
@@ -927,6 +976,8 @@ void FeMedia::close()
 	std::lock_guard<std::mutex> l( m_callback_mutex );
 
 	stop();
+
+	setEffectProcessor( nullptr );
 
 	if (m_audio)
 	{
@@ -967,6 +1018,10 @@ void FeMedia::setVolume(float volume)
 	}
 
 	sf::SoundStream::setVolume( volume );
+
+	auto* normaliser = m_audio_effects.get_effect<FeAudioNormaliser>();
+	if ( normaliser )
+		normaliser->set_media_volume( volume / 100.0f );
 }
 
 int fe_media_read( void *opaque, uint8_t *buff, int buff_size )
@@ -1012,6 +1067,7 @@ size_t fe_media_seek( void *opaque, int64_t offset, int whence )
 bool FeMedia::open( const std::string &archive,
 	const std::string &name, sf::Texture *outt )
 {
+	m_audio_effects.reset_all();
 	close();
 
 	sf::InputStream *s = NULL;
@@ -1116,12 +1172,19 @@ bool FeMedia::open( const std::string &archive,
 				int nb_channels = codec_ctx->channels;
 #endif
 
+				std::vector<sf::SoundChannel> channelMap;
+				channelMap.push_back( sf::SoundChannel::FrontLeft );
+				if ( nb_channels > 1 )
+					channelMap.push_back( sf::SoundChannel::FrontRight );
+
 				sf::SoundStream::initialize(
 					nb_channels,
 					codec_ctx->sample_rate,
-					{ sf::SoundChannel::FrontLeft, sf::SoundChannel::FrontRight } );
+					channelMap );
 
 				sf::SoundStream::setLooping( false );
+
+				setup_effect_processor();
 			}
 		}
 	}
@@ -1294,6 +1357,9 @@ bool FeMedia::tick()
 {
 	if (( !m_video ) && ( !m_audio ))
 		return false;
+
+	if ( m_audio )
+		m_audio_effects.update_all();
 
 	if ( m_video )
 	{
@@ -1587,4 +1653,60 @@ void try_hw_accel( AVCodecContext *&codec_ctx, FeAVCodec *&dec )
 				<< av_hwdevice_get_type_name( fe_hw_accels[i] ) << std::endl;
 	}
 #endif
+}
+
+FeAudioVisualiser* FeMedia::get_audio_visualiser()
+{
+	return m_audio_effects.get_effect<FeAudioVisualiser>();
+}
+
+float FeMedia::get_vu_left()
+{
+	auto* visualiser = get_audio_visualiser();
+	return visualiser ? visualiser->get_vu_left() : 0.0f;
+}
+
+float FeMedia::get_vu_right()
+{
+	auto* visualiser = get_audio_visualiser();
+	return visualiser ? visualiser->get_vu_right() : 0.0f;
+}
+
+float FeMedia::get_vu_mono()
+{
+	auto* visualiser = get_audio_visualiser();
+	return visualiser ? visualiser->get_vu_mono() : 0.0f;
+}
+
+Sqrat::Array FeMedia::get_fft_array_mono()
+{
+	auto* visualiser = get_audio_visualiser();
+	if ( visualiser )
+		return visualiser->get_fft_array_mono();
+
+	// Return empty array if no visualiser
+	HSQUIRRELVM vm = Sqrat::DefaultVM::Get();
+	return Sqrat::Array( vm );
+}
+
+Sqrat::Array FeMedia::get_fft_array_left()
+{
+	auto* visualiser = get_audio_visualiser();
+	if ( visualiser )
+		return visualiser->get_fft_array_left();
+
+	// Return empty array if no visualiser
+	HSQUIRRELVM vm = Sqrat::DefaultVM::Get();
+	return Sqrat::Array( vm );
+}
+
+Sqrat::Array FeMedia::get_fft_array_right()
+{
+	auto* visualiser = get_audio_visualiser();
+	if ( visualiser )
+		return visualiser->get_fft_array_right();
+
+	// Return empty array if no visualiser
+	HSQUIRRELVM vm = Sqrat::DefaultVM::Get();
+	return Sqrat::Array( vm );
 }

Makefile
@@ -151,6 +151,7 @@ _DEP =\
 	fe_image.hpp \
 	fe_sound.hpp \
 	fe_music.hpp \
+	fe_audio_fx.hpp \
 	fe_shader.hpp \
 	fe_overlay.hpp \
 	fe_window.hpp \
@@ -188,6 +189,7 @@ _OBJ =\
 	fe_image.o \
 	fe_sound.o \
 	fe_music.o \
+	fe_audio_fx.o \
 	fe_shader.o \
 	fe_overlay.o \
 	fe_window.o \

config/language/en.msg
@@ -135,6 +135,7 @@ _help_joystick_threshold;The threshold for registering joystick moves, from 1 to
 _help_language;Select the language to use in Attract-Mode's user interface
 _help_layout_file;Some layouts provide multiple layout*.nut files.  Select which layout file to use
 _help_layout_preview;Show layout configuration changes immediately
+_help_loudness_normalisation;Enable an audio effect that automatically boosts quiet media to a consistent volume level
 _help_misc;Configure general settings
 _help_mouse_threshold;The threshold for registering mouse moves, from 1 to 100 (100=least sensitive)
 _help_multiple_monitors;Enable the use of multiple monitors by Attract-Mode.  Setting this to 'No' may reduce screen flicker when launching games

src/fe_audio_fx.hpp
@@ -1,0 +1,197 @@
+/*
+ *
+ *  Attract-Mode Plus frontend
+ *  Copyright (C) 2025 Andrew Mickelson & Radek Dutkiewicz
+ *
+ *  This file is part of Attract-Mode Plus
+ *
+ *  Attract-Mode Plus is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Attract-Mode Plus is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Attract-Mode Plus.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef FE_AUDIO_FX_HPP
+#define FE_AUDIO_FX_HPP
+
+#include <SFML/System.hpp>
+#include <vector>
+#include <memory>
+#include <sqrat.h>
+#include <cmath>
+
+class FeAudioEffect
+{
+public:
+	FeAudioEffect();
+	virtual ~FeAudioEffect() = default;
+
+	// Pure virtual methods that all effects must implement
+	virtual bool process( const float *input_frames, float *output_frames,
+	                           unsigned int frame_count, unsigned int channel_count,
+	                           float media_sample_rate ) = 0;
+	virtual void update() = 0;
+	virtual void reset() = 0;
+
+	// Optional methods that effects can override
+	virtual bool is_enabled() const { return m_enabled; }
+	virtual void set_enabled( bool enabled ) { m_enabled = enabled; }
+
+protected:
+	bool m_enabled = true;
+	float m_device_sample_rate;
+};
+
+
+class FeAudioEffectsManager
+{
+public:
+	FeAudioEffectsManager();
+	~FeAudioEffectsManager();
+
+	void add_effect( std::unique_ptr<FeAudioEffect> effect );
+	bool process_all( const float *input_frames, float *output_frames,
+	                            unsigned int frame_count, unsigned int channel_count,
+	                            float media_sample_rate );
+
+	void update_all();
+	void reset_all();
+
+	// Get specific effect (returns nullptr if not found or wrong type)
+	template<typename T>
+	T* get_effect() const;
+
+private:
+	std::vector<std::unique_ptr<FeAudioEffect>> m_effects;
+};
+
+
+class FeAudioDCFilter : public FeAudioEffect
+{
+public:
+	FeAudioDCFilter( float cutoff_freq = 20.0f );
+	~FeAudioDCFilter() override = default;
+
+	bool process( const float *input_frames, float *output_frames,
+	                   unsigned int frame_count, unsigned int channel_count,
+	                   float media_sample_rate ) override;
+
+	void update() override;
+	void reset() override;
+
+private:
+	float m_cutoff_freq;
+	mutable float m_coefficient;
+	mutable std::vector<float> m_prev_input;  // Previous input samples per channel
+	mutable std::vector<float> m_prev_output; // Previous output samples per channel
+};
+
+
+class FeAudioVisualiser : public FeAudioEffect
+{
+public:
+	FeAudioVisualiser();
+	~FeAudioVisualiser() override;
+
+	bool process( const float *input_frames, float *output_frames,
+	                   unsigned int frame_count, unsigned int channel_count,
+	                   float media_sample_rate ) override;
+
+	void update() override;
+	void reset() override;
+
+	float get_vu_mono() const;
+	float get_vu_left() const;
+	float get_vu_right() const;
+
+	// Sqrat bindings for FFT arrays
+	Sqrat::Array get_fft_array_mono() const;
+	Sqrat::Array get_fft_array_left() const;
+	Sqrat::Array get_fft_array_right() const;
+
+	static const int FFT_BANDS = 32;
+
+private:
+	// VU meter data
+	mutable float m_vu_mono_in;
+	mutable float m_vu_mono_out;
+	mutable float m_vu_left_in;
+	mutable float m_vu_left_out;
+	mutable float m_vu_right_in;
+	mutable float m_vu_right_out;
+
+	// FFT analysis data
+	mutable std::vector<float> m_fft_mono_in;
+	mutable std::vector<float> m_fft_mono_out;
+	mutable std::vector<float> m_fft_left_in;
+	mutable std::vector<float> m_fft_left_out;
+	mutable std::vector<float> m_fft_right_in;
+	mutable std::vector<float> m_fft_right_out;
+
+	mutable sf::Time m_last_frame_time;
+	mutable sf::Clock m_system_clock;
+	mutable bool m_reset_done;
+
+	mutable bool m_vu_requested;
+	mutable bool m_fft_requested;
+
+	// Rolling buffer and resampling for FFT
+	mutable std::vector<float> m_rolling_buffer_mono;
+	mutable std::vector<float> m_rolling_buffer_left;
+	mutable std::vector<float> m_rolling_buffer_right;
+	mutable std::vector<float> m_window_lut;
+	mutable size_t m_buffer_write_pos;
+	mutable size_t m_buffer_samples_count;
+	mutable double m_phase_accumulator;
+
+	void calculate_fft_channel( const float *samples , unsigned int sample_count ,
+	                            std::vector<float> &fft_bands , float sample_rate ) const;
+	void update_fall() const;
+	float convert_to_log_scale( float linear_value, float amplitude_linearity ) const;
+	bool resample_and_buffer_audio( const float* input_frames, unsigned int input_frame_count,
+	                                unsigned int frame_channel_count );
+};
+
+class FeAudioNormaliser : public FeAudioEffect
+{
+public:
+	FeAudioNormaliser();
+	~FeAudioNormaliser() override = default;
+
+	bool process( const float *input_frames, float *output_frames,
+	                   unsigned int frame_count, unsigned int channel_count,
+	                   float media_sample_rate ) override;
+
+	void update() override;
+	void reset() override;
+
+	void set_media_volume( float volume );
+
+private:
+	mutable float m_current_gain = 1.0f;
+	mutable bool m_target_reached = false;
+	mutable size_t m_startup_delay = 0;
+	float m_media_volume = 1.0f;
+};
+
+template<typename T>
+T* FeAudioEffectsManager::get_effect() const
+{
+	for ( const auto& effect : m_effects )
+	{
+		if ( T* casted = dynamic_cast<T*>( effect.get() ) )
+			return casted;
+	}
+	return nullptr;
+}
+
+#endif // FE_AUDIO_FX_HPP

src/fe_config.cpp
@@ -1585,6 +1585,9 @@ void FeSoundMenu::get_options( FeConfigContext &ctx )
 		int v = ctx.fe_settings.get_set_volume( (FeSoundInfo::SoundType)i );
 		ctx.add_opt( Opt::LIST, FeSoundInfo::settingDispStrings[i], as_str(v), "_help_volume" )->append_vlist( volumes );
 	}
+	// Loudness Normalisation toggle
+	//
+	ctx.add_opt( Opt::TOGGLE, FeSoundInfo::settingDispStrings[3], ctx.fe_settings.get_loudness(), "_help_loudness_normalisation" );
 
 	//
 	// Get the list of available sound files
@@ -1635,9 +1638,14 @@ bool FeSoundMenu::save( FeConfigContext &ctx )
 		);
 
 	//
+	// Loudness Normalisation
+	//
+	ctx.fe_settings.set_loudness( ctx.opt_list[3].get_vindex() == 0 );
+
+	//
 	// Save the sound settings
 	//
-	for ( i=3; i< (int)ctx.opt_list.size(); i++ )
+	for ( i=4; i< (int)ctx.opt_list.size(); i++ )
 	{
 		if ( ctx.opt_list[i].opaque >= 0 )
 			ctx.fe_settings.set_sound_file(

src/fe_image.cpp
@@ -26,6 +26,8 @@
 #include "fe_shader.hpp"
 #include "fe_present.hpp"
 #include "fe_blend.hpp"
+#include "fe_vm.hpp"
+#include "fe_audio_fx.hpp"
 #include "zip.hpp"
 #include "image_loader.hpp"
 #include <cmath>
@@ -574,41 +576,44 @@ bool FeTextureContainer::tick( FeSettings *feSettings, bool play_movies )
 		return false;
 
 #ifndef NO_MOVIE
-	if (( m_movie ) && ( m_movie_status > 0 ))
+	if ( m_movie )
 	{
-		if ( m_movie_status < PLAY_COUNT )
+		if ( m_movie_status > 0 )
 		{
-			//
-			// We skip the first few "ticks" after the movie
-			// is first loaded because the user may just be
-			// scrolling rapidly through the game list (there
-			// are ticks between each selection scrolling by)
-			//
-			m_movie_status++;
-			return false;
-		}
-		else if ( m_movie_status == PLAY_COUNT )
-		{
-			m_movie_status++;
+			if ( m_movie_status < PLAY_COUNT )
+			{
+				//
+				// We skip the first few "ticks" after the movie
+				// is first loaded because the user may just be
+				// scrolling rapidly through the game list (there
+				// are ticks between each selection scrolling by)
+				//
+				m_movie_status++;
+				return false;
+			}
+			else if ( m_movie_status == PLAY_COUNT )
+			{
+				m_movie_status++;
 
-			//
-			// Start playing now if this is a video...
-			//
-			if ( m_video_flags & VF_NoAudio )
-				m_movie->setVolume( 0.f );
-			else
-				m_movie->setVolume( m_volume * feSettings->get_play_volume( FeSoundInfo::Movie ) / 100.0 );
+				//
+				// Start playing now if this is a video...
+				//
+				if ( m_video_flags & VF_NoAudio )
+					m_movie->setVolume( 0.f );
+				else
+					m_movie->setVolume( m_volume * feSettings->get_play_volume( FeSoundInfo::Movie ) / 100.0 );
 
-			m_movie->play();
-		}
+				m_movie->play();
+			}
 
-		// restart looped video
-		if ( !(m_video_flags & VF_NoLoop) && !m_movie->is_playing() )
-		{
-			m_movie->stop();
-			m_movie->play();
+			// restart looped video
+			if ( !(m_video_flags & VF_NoLoop) && !m_movie->is_playing() )
+			{
+				m_movie->stop();
+				m_movie->play();
 
-			FeDebug() << "Restarted looped video" << std::endl;
+				FeDebug() << "Restarted looped video" << std::endl;
+			}
 		}
 
 		if ( m_movie->tick() )
@@ -889,6 +894,15 @@ float FeTextureContainer::get_sample_aspect_ratio() const
 		return m_movie->get_aspect_ratio();
 #endif
 		return 1.0;
+}
+
+FeMedia *FeTextureContainer::get_media() const
+{
+#ifndef NO_MOVIE
+	return m_movie;
+#else
+	return NULL;
+#endif
 }
 
 void FeTextureContainer::release_audio( bool state )
@@ -1813,6 +1827,93 @@ void FeImage::set_volume( float v )
 float FeImage::get_volume() const
 {
 	return m_tex->get_volume();
+}
+
+float FeImage::get_vu_mono() const
+{
+	FeTextureContainer *tc = dynamic_cast<FeTextureContainer*>(m_tex);
+	if ( tc )
+	{
+		FeMedia *media = tc->get_media();
+		if ( media )
+			return media->get_vu_mono();
+	}
+	return 0.0f;
+}
+
+float FeImage::get_vu_left() const
+{
+	FeTextureContainer *tc = dynamic_cast<FeTextureContainer*>(m_tex);
+	if ( tc )
+	{
+		FeMedia *media = tc->get_media();
+		if ( media )
+			return media->get_vu_left();
+	}
+	return 0.0f;
+}
+
+float FeImage::get_vu_right() const
+{
+	FeTextureContainer *tc = dynamic_cast<FeTextureContainer*>(m_tex);
+	if ( tc )
+	{
+		FeMedia *media = tc->get_media();
+		if ( media )
+			return media->get_vu_right();
+	}
+	return 0.0f;
+}
+
+Sqrat::Array FeImage::get_fft_array_mono()
+{
+	FeTextureContainer *tc = dynamic_cast<FeTextureContainer*>( m_tex );
+	if ( tc )
+	{
+		FeMedia *media = tc->get_media();
+		if ( media )
+			return media->get_fft_array_mono();
+	}
+
+	Sqrat::Array arr( Sqrat::DefaultVM::Get(), FeAudioVisualiser::FFT_BANDS );
+	for ( int i = 0; i < FeAudioVisualiser::FFT_BANDS; ++i )
+		arr.SetValue( i, 0.0f );
+
+	return arr;
+}
+
+Sqrat::Array FeImage::get_fft_array_left()
+{
+	FeTextureContainer *tc = dynamic_cast<FeTextureContainer*>( m_tex );
+	if ( tc )
+	{
+		FeMedia *media = tc->get_media();
+		if ( media )
+			return media->get_fft_array_left();
+	}
+
+	Sqrat::Array arr( Sqrat::DefaultVM::Get(), FeAudioVisualiser::FFT_BANDS );
+	for ( int i = 0; i < FeAudioVisualiser::FFT_BANDS; ++i )
+		arr.SetValue( i, 0.0f );
+
+	return arr;
+}
+
+Sqrat::Array FeImage::get_fft_array_right()
+{
+	FeTextureContainer *tc = dynamic_cast<FeTextureContainer*>( m_tex );
+	if ( tc )
+	{
+		FeMedia *media = tc->get_media();
+		if ( media )
+			return media->get_fft_array_right();
+	}
+
+	Sqrat::Array arr( Sqrat::DefaultVM::Get(), FeAudioVisualiser::FFT_BANDS );
+	for ( int i = 0; i < FeAudioVisualiser::FFT_BANDS; ++i )
+		arr.SetValue( i, 0.0f );
+
+	return arr;
 }
 
 void FeImage::transition_swap( FeImage *o )

commit 3f370a89cb979562c6d5189f13b9fea1ce1d8239

Fix segfaults in get_fes()
Regression caused by a1bada2b9fbd602dd090b4af162b0001613a95d2

src/fe_audio_fx.cpp
@@ -120,6 +120,14 @@ bool FeAudioEffectsManager::process_all( const float *input_frames, float *outpu
 
 void FeAudioEffectsManager::update_all()
 {
+	FePresent *fep = FePresent::script_get_fep();
+	if ( fep )
+	{
+		auto* normaliser = get_effect<FeAudioNormaliser>();
+		if ( normaliser )
+			normaliser->set_enabled( fep->get_fes()->get_loudness() );
+	}
+
 	for ( auto& effect : m_effects )
 	{
 		if ( effect && effect->is_enabled() )

src/fe_music.cpp
@@ -63,16 +63,6 @@ FeMusic::FeMusic( bool loop )
 	                                    float *output_frames, unsigned int &output_frame_count,
 	                                    unsigned int frame_channel_count )
 	{
-		FePresent *fep = FePresent::script_get_fep();
-		if ( fep )
-		{
-			auto* normaliser = m_audio_effects.get_effect<FeAudioNormaliser>();
-			if ( normaliser )
-			{
-				normaliser->set_enabled( fep->get_fes()->get_loudness() );
-			}
-		}
-
 		if ( input_frames && input_frame_count > 0 && get_playing() )
 		{
 			m_audio_effects.process_all( input_frames, output_frames,

src/media.cpp
@@ -871,16 +871,6 @@ void FeMedia::setup_effect_processor()
 	                            float *output_frames, unsigned int &output_frame_count,
 	                            unsigned int frame_channel_count )
 	{
-		FePresent *fep = FePresent::script_get_fep();
-		if ( fep )
-		{
-			auto* normaliser = m_audio_effects.get_effect<FeAudioNormaliser>();
-			if ( normaliser )
-			{
-				normaliser->set_enabled( fep->get_fes()->get_loudness() );
-			}
-		}
-
 		if ( input_frames && input_frame_count > 0 && is_playing() )
 		{
 			m_audio_effects.process_all( input_frames, output_frames,