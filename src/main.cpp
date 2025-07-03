oomek/attractplus

commit b218ca33d4ac855b89ace0dd5753f81508591492

Fix back button behaviour in Displays Menu
When Displays Menu has custom layout set and Startup Mode is not set as Show Displays Menu after pressing back button return to previously active Display instead of exiting AM+

src/main.cpp
@@ -614,6 +614,19 @@ int main(int argc, char *argv[])
 				{
 				case FeInputMap::Exit:
 					{
+						if ( feSettings.get_startup_mode() != FeSettings::ShowDisplaysMenu && feSettings.has_custom_displays_menu() )
+						{
+							int display_index = feSettings.get_current_display_index();
+							if (display_index == -1 && last_display_index != -1)
+							{
+								display_index = last_display_index;
+								last_display_index = -1;
+								feSettings.set_display( display_index );
+								feVM.load_layout( true );
+								redraw=true;
+								break;
+							}
+						}
 						if ( feOverlay.common_exit() )
 							exit_selected=true;
 

commit d46f6fc9a52d408dc49868fc91d0e0277e4519a7

Fix back button behaviour
Fixes issue #188
Regression caused by b218ca33d4ac855b89ace0dd5753f81508591492

src/main.cpp
@@ -497,6 +497,23 @@ int main(int argc, char *argv[])
 					}
 
 					//
+					// If we're in a custom Displays Menu, go back to the previous display
+					//
+					if ( feSettings.has_custom_displays_menu() )
+					{
+						int display_index = feSettings.get_current_display_index();
+						if ( display_index == -1 && last_display_index != -1 )
+						{
+							display_index = last_display_index;
+							last_display_index = -1;
+							feSettings.set_display( display_index );
+							feVM.load_layout( true );
+							redraw=true;
+							continue;
+						}
+					}
+
+					//
 					// If FE is configured to show displays menu on startup, then the "Back" UI
 					// button goes back to that menu accordingly...
 					//
@@ -619,7 +636,7 @@ int main(int argc, char *argv[])
 						if ( feSettings.get_startup_mode() != FeSettings::ShowDisplaysMenu && feSettings.has_custom_displays_menu() )
 						{
 							int display_index = feSettings.get_current_display_index();
-							if (display_index == -1 && last_display_index != -1)
+							if ( display_index == -1 && last_display_index != -1 && !feSettings.get_info_bool( FeSettings::DisplaysMenuExit ))
 							{
 								display_index = last_display_index;
 								last_display_index = -1;