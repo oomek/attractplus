oomek/attractplus

commit 017f59d7f5fecd5644f55d31eac4d7dea0b6ac3b

Fix back button behaviour
Fixes issue #188
Regression caused by b218ca33d4ac855b89ace0dd5753f81508591492

Bring back m_actual_display_index
Regression caused by c303acb9e7702e1c9331aa3de5b381bb12101848

src/main.cpp
@@ -497,6 +497,23 @@ int main(int argc, char *argv[])
 					}
 
 					//
+					// If we're in a custom Displays Menu, go back to the previous display
+					//
+					if ( feSettings.has_custom_displays_menu() )
+					{
+						int display_index = feSettings.get_current_display_index();
+						if ( display_index == -1 && last_display_index != -1 )
+						{
+							display_index = last_display_index;
+							last_display_index = -1;
+							feSettings.set_display( display_index );
+							feVM.load_layout( true );
+							redraw=true;
+							continue;
+						}
+					}
+
+					//
 					// If FE is configured to show displays menu on startup, then the "Back" UI
 					// button goes back to that menu accordingly...
 					//
@@ -619,7 +636,7 @@ int main(int argc, char *argv[])
 						if ( feSettings.get_startup_mode() != FeSettings::ShowDisplaysMenu && feSettings.has_custom_displays_menu() )
 						{
 							int display_index = feSettings.get_current_display_index();
-							if (display_index == -1 && last_display_index != -1)
+							if ( display_index == -1 && last_display_index != -1 && !feSettings.get_info_bool( FeSettings::DisplaysMenuExit ))
 							{
 								display_index = last_display_index;
 								last_display_index = -1;

src/fe_settings.cpp
@@ -319,6 +319,7 @@ FeSettings::FeSettings( const std::string &config_path )
 	m_display_menu_per_display_params( FeLayoutInfo::Menu ),
 	m_current_display( -1 ),
 	m_selected_display( 0 ),
+	m_actual_display_index( 0 ),
 	m_current_config_object( NULL ),
 	m_ssaver_time( 600 ),
 	m_last_launch_display( 0 ),
@@ -768,7 +769,7 @@ void FeSettings::construct_display_maps()
 
 void FeSettings::save_state()
 {
-	int display_idx = m_current_display;
+	int display_idx = m_actual_display_index;
 
 	// If we have a display stack we save the entry at the bottom of the stack as the
 	// 'current' display
@@ -870,6 +871,7 @@ void FeSettings::load_state()
 		m_current_display = 0;
 
 	m_selected_display = m_current_display;
+	m_actual_display_index = m_current_display;
 
 	// bound checking on the last launch state
 	if ( m_last_launch_display >= (int)m_displays.size() )
@@ -1558,7 +1560,10 @@ bool FeSettings::set_display( int index, bool stack_previous )
 	m_current_display = index;
 
 	if ( m_current_display != -1 )
+	{
+		m_actual_display_index = m_current_display;
 		m_selected_display = m_current_display;
+	}
 
 	m_rl.save_state();
 	init_display();
@@ -1641,6 +1646,11 @@ bool FeSettings::navigate_filter( int step )
 int FeSettings::get_current_display_index() const
 {
 	return m_current_display;
+}
+
+int FeSettings::get_actual_display_index() const
+{
+	return m_actual_display_index;
 }
 
 int FeSettings::get_selected_display_index() const

src/fe_settings.hpp
@@ -220,6 +220,7 @@ private:
 
 	int m_current_display; // The index of the current display, -1 if showing a custom displays_menu
 	int m_selected_display; // The index of the displays_menu selected display, -1 if EXIT is selected. Equals m_current_display if displays_menu not shown.
+	int m_actual_display_index; // The most recent actual display index that was selected (for saving)
 	FeBaseConfigurable *m_current_config_object;
 	int m_ssaver_time;
 	int m_last_launch_display;
@@ -378,6 +379,7 @@ public:
 	bool back_displays_available() { return !m_display_stack.empty(); };
 
 	int get_current_display_index() const;
+	int get_actual_display_index() const;
 	int get_selected_display_index() const;
 	int get_display_index_from_name( const std::string &name ) const;
 	int displays_count() const;

commit 8c5280afefed16636469bedb44b7cc1ff73d4bb0

Restore previous behaviour of the back button
Regression caused by 017f59d7f5fecd5644f55d31eac4d7dea0b6ac3b

src/main.cpp
@@ -497,23 +497,6 @@ int main(int argc, char *argv[])
 					}
 
 					//
-					// If we're in a custom Displays Menu, go back to the previous display
-					//
-					if ( feSettings.has_custom_displays_menu() )
-					{
-						int display_index = feSettings.get_current_display_index();
-						if ( display_index == -1 && last_display_index != -1 )
-						{
-							display_index = last_display_index;
-							last_display_index = -1;
-							feSettings.set_display( display_index );
-							feVM.load_layout( true );
-							redraw=true;
-							continue;
-						}
-					}
-
-					//
 					// If FE is configured to show displays menu on startup, then the "Back" UI
 					// button goes back to that menu accordingly...
 					//
@@ -640,7 +623,7 @@ int main(int argc, char *argv[])
 						if ( feSettings.get_startup_mode() != FeSettings::ShowDisplaysMenu && feSettings.has_custom_displays_menu() )
 						{
 							int display_index = feSettings.get_current_display_index();
-							if ( display_index == -1 && last_display_index != -1 && !feSettings.get_info_bool( FeSettings::DisplaysMenuExit ))
+							if ( display_index == -1 && last_display_index != -1 )
 							{
 								display_index = last_display_index;
 								last_display_index = -1;