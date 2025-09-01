oomek/attractplus

commit 85d76e54b8509aa8b83e4668a306bc3043485ece

Use m_layoutTimer in FeAudioVisualiser::update_fall()

src/fe_audio_fx.cpp
@@ -620,13 +620,12 @@ void FeAudioVisualiser::update_fall() const
 {
 	sf::Time current_time = m_system_clock.getElapsedTime();
 
-	float delta_time = ( current_time - m_last_frame_time ).asSeconds();
-	m_last_frame_time = current_time;
-
-	float refresh_rate = 60.0f;
 	FePresent *fep = FePresent::script_get_fep();
 	if ( fep )
-		refresh_rate = static_cast<float>( fep->get_refresh_rate() );
+		current_time = fep->get_layout_time();
+
+	float delta_time = ( current_time - m_last_frame_time ).asSeconds();
+	m_last_frame_time = current_time;
 
 	float vu_fall_amount = VU_FALL_SPEED * delta_time;
 	float fft_fall_amount = FFT_FALL_SPEED * delta_time;

src/fe_present.cpp
@@ -1680,6 +1680,11 @@ int FePresent::get_layout_ms()
 	return m_layoutTimer.getElapsedTime().asMilliseconds();
 }
 
+sf::Time FePresent::get_layout_time()
+{
+	return m_layoutTimer.getElapsedTime();
+}
+
 int FePresent::get_refresh_rate()
 {
 	return m_refresh_rate;

src/fe_present.hpp
@@ -277,6 +277,7 @@ public:
 	bool get_video_toggle() { return m_playMovies; };
 
 	int get_layout_ms();
+	sf::Time get_layout_time();
 	int get_refresh_rate();
 	bool get_mouse_pointer();
 	void set_mouse_pointer( bool );

commit bb5ad1130edd5ae337c0884533941c2c1c914f4b

Fix issue #229
Regression caused by 85d76e54b8509aa8b83e4668a306bc3043485ece

src/fe_audio_fx.cpp
@@ -375,28 +375,40 @@ void FeAudioVisualiser::update()
 float FeAudioVisualiser::get_vu_mono() const
 {
 	m_vu_requested = true;
-	m_vu_request_time = m_system_clock.getElapsedTime();
+	FePresent *fep = FePresent::script_get_fep();
+	if ( fep )
+		m_vu_request_time = fep->get_layout_time();
+
 	return m_vu_mono_out;
 }
 
 float FeAudioVisualiser::get_vu_left() const
 {
 	m_vu_requested = true;
-	m_vu_request_time = m_system_clock.getElapsedTime();
+	FePresent *fep = FePresent::script_get_fep();
+	if ( fep )
+		m_vu_request_time = fep->get_layout_time();
+
 	return m_vu_left_out;
 }
 
 float FeAudioVisualiser::get_vu_right() const
 {
 	m_vu_requested = true;
-	m_vu_request_time = m_system_clock.getElapsedTime();
+	FePresent *fep = FePresent::script_get_fep();
+	if ( fep )
+		m_vu_request_time = fep->get_layout_time();
+
 	return m_vu_right_out;
 }
 
 Sqrat::Array FeAudioVisualiser::get_fft_array_mono() const
 {
 	m_fft_requested = true;
-	m_fft_request_time = m_system_clock.getElapsedTime();
+	FePresent *fep = FePresent::script_get_fep();
+	if ( fep )
+		m_fft_request_time = fep->get_layout_time();
+
 	HSQUIRRELVM vm = Sqrat::DefaultVM::Get();
 
 	Sqrat::Table fft_table( vm );
@@ -414,7 +426,10 @@ Sqrat::Array FeAudioVisualiser::get_fft_array_mono() const
 Sqrat::Array FeAudioVisualiser::get_fft_array_left() const
 {
 	m_fft_requested = true;
-	m_fft_request_time = m_system_clock.getElapsedTime();
+	FePresent *fep = FePresent::script_get_fep();
+	if ( fep )
+		m_fft_request_time = fep->get_layout_time();
+
 	HSQUIRRELVM vm = Sqrat::DefaultVM::Get();
 
 	Sqrat::Table fft_table( vm );
@@ -432,7 +447,10 @@ Sqrat::Array FeAudioVisualiser::get_fft_array_left() const
 Sqrat::Array FeAudioVisualiser::get_fft_array_right() const
 {
 	m_fft_requested = true;
-	m_fft_request_time = m_system_clock.getElapsedTime();
+	FePresent *fep = FePresent::script_get_fep();
+	if ( fep )
+		m_fft_request_time = fep->get_layout_time();
+
 	HSQUIRRELVM vm = Sqrat::DefaultVM::Get();
 
 	Sqrat::Table fft_table( vm );
@@ -636,17 +654,18 @@ bool FeAudioVisualiser::resample_and_buffer_audio( const float* input_frames, un
 
 void FeAudioVisualiser::update_fall() const
 {
-	sf::Time current_time = m_system_clock.getElapsedTime();
+	sf::Time current_time =  sf::Time::Zero;
+	float frame_time = 0.0;
 
 	FePresent *fep = FePresent::script_get_fep();
 	if ( fep )
+	{
 		current_time = fep->get_layout_time();
+		frame_time = fep->get_layout_frame_time() * 0.001f;
+	}
 
-	float delta_time = ( current_time - m_last_frame_time ).asSeconds();
-	m_last_frame_time = current_time;
-
-	float vu_fall_amount = VU_FALL_SPEED * delta_time;
-	float fft_fall_amount = FFT_FALL_SPEED * delta_time;
+	float vu_fall_amount = VU_FALL_SPEED * frame_time;
+	float fft_fall_amount = FFT_FALL_SPEED * frame_time;
 
 	// Lambda for fall-off calculation ( compiler inlined, faster than a function call )
 	auto apply_vu_fall = []( float input_value, float& output_value, float fall_amount )