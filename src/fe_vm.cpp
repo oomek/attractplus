oomek/attractplus

commit 73e7f0a1d3722b8acde4cef665a9a0170246c2f8

Add Layout NV

src/fe_vm.cpp
@@ -61,6 +61,7 @@
 #include <algorithm>
 
 const char *FE_SCRIPT_NV_FILE = "script.nv";
+const char *FE_LAYOUT_NV_FILE = "layout.nv";
 
 namespace
 {
@@ -196,41 +197,6 @@ namespace
 
 		return 1;
 	}
-
-	std::string read_non_volatile_to_string( const SQChar *name )
-	{
-		Sqrat::Table fe( Sqrat::RootTable().GetSlot( _SC("fe") ) );
-		Sqrat::Table snv( fe.GetSlot( name ) );
-
-		if ( snv.IsNull() )
-			return "";
-
-		return fe_to_json_string( snv );
-	}
-
-	void write_non_volatile_from_string( const char *name,
-		std::string &nv )
-	{
-		if ( !nv.empty() )
-		{
-			std::string temp = "fe.";
-			temp += name;
-			temp += " <- ";
-			temp += nv;
-			temp += ";";
-
-			try
-			{
-				Sqrat::Script sc;
-				sc.CompileString( temp );
-				sc.Run();
-			}
-			catch ( const Sqrat::Exception &e )
-			{
-				FeLog() << "Error compiling " << name << " string: [" << nv << "] - " << e.Message() << std::endl;
-			}
-		}
-	}
 };
 
 FeCallback::FeCallback( int pid,
@@ -296,47 +262,64 @@ FeVM::FeVM( FeSettings &fes, FeWindow &wnd, FeMusic &ambient_sound, bool console
 	srand( time( NULL ) );
 	vm_init();
 
-	//
-	// Read the "non-volatile" table (fe.nv) from the filesystem now
-	//
-	std::string filename = m_feSettings->get_config_dir();
-	filename += FE_SCRIPT_NV_FILE;
-
-	nowide::ifstream infile( filename.c_str() );
-	if ( !infile.is_open() )
-		return;
-
-	std::string nv;
-	while ( infile.good() )
-	{
-		std::string line;
-		getline( infile, line );
-		nv += line;
-	}
-
 	Sqrat::Table fe;
-	Sqrat::RootTable().Bind( _SC("fe"),  fe );
-	write_non_volatile_from_string( "nv", nv );
+	Sqrat::RootTable().Bind( _SC("fe"), fe );
+	load_script_nv();
 }
 
 FeVM::~FeVM()
 {
 	clear_handlers();
-
-	//
-	// Save the "non-volatile" squirrel table (fe.nv) to a file now
-	//
-	std::string filename = m_feSettings->get_config_dir();
-	filename += FE_SCRIPT_NV_FILE;
-
-	nowide::ofstream outfile( filename.c_str() );
-	if ( outfile.is_open() )
-	{
-		outfile << read_non_volatile_to_string( _SC( "nv" ) ) << std::endl;
-		outfile.close();
-	}
-
+	save_script_nv();
+	save_layout_nv();
 	vm_close();
+}
+
+// Load fe.nv from file
+void FeVM::load_script_nv() {
+	std::string nv;
+	read_file_content( m_feSettings->get_config_dir() + FE_SCRIPT_NV_FILE, nv );
+	sq_run_code( "fe.nv <- " + ( nv.empty() ? "{}" : nv ) );
+}
+
+// Save fe.nv to file
+void FeVM::save_script_nv() {
+	write_file_content( m_feSettings->get_config_dir() + FE_SCRIPT_NV_FILE, sq_slot_to_json( "fe.nv" ) );
+}
+
+// Load the fe.layout.nv for the current layout
+void FeVM::load_layout_nv() {
+	FeDisplayInfo *di = m_feSettings->get_display( m_feSettings->get_current_display_index() );
+	m_last_layout = di->get_layout();
+
+	std::string layout_nv;
+	read_file_content( m_feSettings->get_config_dir() + FE_LAYOUT_NV_FILE, layout_nv );
+	if ( layout_nv.empty() ) layout_nv = "{}";
+
+	std::string key = sq_escape_string( m_last_layout );
+	sq_run_code( "try { fe.layout.nv = " + layout_nv + "[\"" + key + "\"] } catch (err) { fe.layout.nv = {} }" );
+}
+
+// Save the fe.layout.nv for the *last loaded* layout
+void FeVM::save_layout_nv() {
+	if ( m_last_layout.empty() )
+		return;
+
+	std::string layout_nv;
+	read_file_content( m_feSettings->get_config_dir() + FE_LAYOUT_NV_FILE, layout_nv );
+	if ( layout_nv.empty() ) layout_nv = "{}";
+
+	// All layout nv's are stored in one table
+	// - Rebuild the full table in a temp variable, save it, then remove it
+	std::string temp = "_fe_layout_nv";
+	std::string key = sq_escape_string( m_last_layout );
+	sq_run_code( "try { " + temp + " <- " + layout_nv + "\n" + temp + "[\"" + key + "\"] <- fe.layout.nv } catch(err) {}" );
+	std::string json = sq_slot_to_json( temp );
+	if ( json.empty() )
+		return;
+
+	write_file_content( m_feSettings->get_config_dir() + FE_LAYOUT_NV_FILE, json );
+	sq_run_code( "try { delete " + temp + " } catch (err) {}" );
 }
 
 void FeVM::set_overlay( FeOverlay *feo )
@@ -523,10 +506,12 @@ bool FeVM::on_new_layout()
 		= m_feSettings->get_current_config( FeSettings::Current );
 
 
-	// Grab the contents of the existing "non-volatile" squirrel table
-	// before reinitializing
+	// Grab the contents of the existing "non-volatile" squirrel table before reinitializing
 	//
-	std::string nv = read_non_volatile_to_string( _SC( "nv" ) );
+	std::string nv = sq_slot_to_json( "fe.nv" );
+
+	save_script_nv();
+	save_layout_nv();
 
 	// Squirrel VM gets reinitialized on each layout
 	//
@@ -940,6 +925,10 @@ bool FeVM::on_new_layout()
 		.Func(_SC("redraw"), &FePresent::redraw )
 	);
 
+	// Create a slot for fe.layout.nv data
+	Sqrat::Table layoutGlobals( fe.GetSlot( _SC("LayoutGlobals") ) );
+	layoutGlobals.SetValue( _SC("nv"), Table() );
+
 	fe.Bind( _SC("CurrentList"), Class <FePresent, NoConstructor>()
 		.Prop( _SC("name"), &FePresent::get_display_name )
 		.Prop( _SC("display_index"), &FePresent::get_display_index )
@@ -1180,11 +1169,6 @@ bool FeVM::on_new_layout()
 	fe.SetInstance( _SC("image_cache"), &il );
 	fe.SetValue( _SC("plugin"), Table() ); // an empty table for plugins to use/abuse
 
-	// We keep a "non-volatile" table for use by layouts/plugins, the
-	// string content of which gets copied over from layout to layout
-	//
-	fe.SetValue( _SC("nv"), Table() );
-
 	// Each presentation object gets an instance in the
 	// "obj" array available in Squirrel
 	//
@@ -1192,7 +1176,13 @@ bool FeVM::on_new_layout()
 	fe.Bind( _SC("obj"), obj );
 	RootTable().Bind( _SC("fe"),  fe );
 
-	write_non_volatile_from_string( "nv", nv );
+	// We keep a "non-volatile" table for use by layouts/plugins, the
+	// string content of which gets copied over from layout to layout
+	//
+	fe.SetValue( _SC("nv"), Table() );
+	sq_run_code( "fe.nv <- " + (nv.empty() ? "{}" : nv) );
+
+	load_layout_nv();
 
 	//
 	// Run any plugin script(s), skip for intro

src/fe_util.cpp
@@ -284,7 +284,7 @@ bool directory_exists( const std::string &file )
 	return check_path( file ) & FeVM::IsDirectory;
 }
 
-bool get_file_content( const std::string &file, std::string &output )
+bool read_file_content( const std::string &file, std::string &content )
 {
 	std::ifstream f( file, std::ios::binary );
 	if ( !f.is_open() )
@@ -293,7 +293,18 @@ bool get_file_content( const std::string &file, std::string &output )
 	std::stringstream buffer;
 	buffer << f.rdbuf();
 	f.close();
-	output = buffer.str();
+	content = buffer.str();
+	return true;
+}
+
+bool write_file_content( const std::string &file, const std::string content )
+{
+	nowide::ofstream f( file );
+	if ( !f.is_open() )
+		return false;
+
+	f << content;
+	f.close();
 	return true;
 }
 

src/fe_util.hpp
@@ -202,8 +202,11 @@ bool file_exists( const std::string &file );
 // return true if specified path is an existing directory
 bool directory_exists( const std::string &file );
 
-// get the contents of the given file, return true on success
-bool get_file_content( const std::string &file, std::string &output );
+// read the contents of the given file, return true on success
+bool read_file_content( const std::string &file, std::string &content );
+
+// write the contents to the given file, return true on success
+bool write_file_content( const std::string &file, const std::string content );
 
 // Check if provided path exists and whether it's a file or a directory
 // Returns one of the following: IsFile, IsDirectory, IsNotFound

src/fe_util_sq.cpp
@@ -185,14 +185,14 @@ std::string escape_char( char c )
 }
 
 // Escape all squirrel special chars in given value
-void escape_string( std::string &value )
+std::string sq_escape_string( const std::string value )
 {
 	std::string out = "";
 	for (char c: value) out += escape_char( c );
-	value = out;
+	return out;
 }
 
-std::string fe_to_json_string( HSQOBJECT obj, int indent )
+std::string sq_obj_to_json( HSQOBJECT obj, int indent )
 {
 	std::string retval;
 	Sqrat::Object sobj( obj );
@@ -206,19 +206,15 @@ std::string fe_to_json_string( HSQOBJECT obj, int indent )
 	case OT_BOOL:
 	case OT_INTEGER:
 	case OT_FLOAT:
-		fe_get_object_string( Sqrat::DefaultVM::Get(),
-			obj, retval );
+		fe_get_object_string( Sqrat::DefaultVM::Get(), obj, retval );
 		break;
 
 	case OT_STRING:
 		retval = "\"";
 		{
 			std::string obj_string;
-			fe_get_object_string( Sqrat::DefaultVM::Get(),
-				obj, obj_string );
-
-			escape_string( obj_string );
-			retval += obj_string;
+			fe_get_object_string( Sqrat::DefaultVM::Get(), obj, obj_string );
+			retval += sq_escape_string( obj_string );
 		}
 		retval += "\"";
 		break;
@@ -234,8 +230,7 @@ std::string fe_to_json_string( HSQOBJECT obj, int indent )
 			bool got_obj = sobj.Next( it );
 			while ( got_obj )
 			{
-				std::string val = fe_to_json_string( it.getValue(),
-						indent+1 );
+				std::string val = sq_obj_to_json( it.getValue(), indent + 1 );
 
 				//
 				// Ignore completely if val comes back empty
@@ -246,7 +241,7 @@ std::string fe_to_json_string( HSQOBJECT obj, int indent )
 					continue;
 				}
 
-				std::string key = fe_to_json_string( it.getKey(), 0 );
+				std::string key = sq_obj_to_json( it.getKey(), 0 );
 
 				retval += "\t";
 				retval += key;
@@ -272,8 +267,7 @@ std::string fe_to_json_string( HSQOBJECT obj, int indent )
 			bool first=true;
 			while ( sobj.Next( it ) )
 			{
-				std::string val = fe_to_json_string( it.getValue(),
-						indent );
+				std::string val = sq_obj_to_json( it.getValue(), indent );
 
 				if ( !val.empty() )
 				{
@@ -305,3 +299,34 @@ std::string fe_to_json_string( HSQOBJECT obj, int indent )
 	return retval;
 }
 
+// Convert squirrel table slot to json
+std::string sq_slot_to_json( std::string name )
+{
+	size_t pos=0;
+	std::string token;
+	Sqrat::Table table = Sqrat::RootTable();
+	name += ".";
+	while ( token_helper( name, pos, token, "." ) )
+	{
+		table = table.GetSlot( scsqchar( token ) );
+		if ( table.IsNull() )
+			return "";
+	}
+
+	return sq_obj_to_json( table );
+}
+
+// Run squirrel code
+void sq_run_code( std::string code )
+{
+	try
+	{
+		Sqrat::Script sc;
+		sc.CompileString( code );
+		sc.Run();
+	}
+	catch ( const Sqrat::Exception &e )
+	{
+		FeLog() << "Error compiling " << code << " - " << e.Message() << std::endl;
+	}
+}

src/fe_util_sq.hpp
@@ -25,6 +25,7 @@
 
 #include <squirrel.h>
 #include <string>
+#include "fe_base.hpp"
 
 //
 // Squirrel utility functions.
@@ -75,6 +76,9 @@ void fe_register_global_func(
 	SQFUNCTION f,
 	const char *name );
 
-std::string fe_to_json_string( HSQOBJECT obj, int indent=0 );
+std::string sq_obj_to_json( HSQOBJECT obj, int indent=0 );
+std::string sq_escape_string( const std::string value );
+std::string sq_slot_to_json( std::string name );
+void sq_run_code( std::string code );
 
 #endif

src/fe_vm.hpp
@@ -92,6 +92,7 @@ private:
 	const FeScriptConfigurable *m_script_cfg;
 	int m_script_id;
 	sf::Time m_last_ui_cmd;
+	std::string m_last_layout;
 
 	std::queue< FeInputMap::Command > m_posted_commands;
 	std::vector< FeCallback > m_ticks;
@@ -113,6 +114,11 @@ private:
 public:
 	FeVM( FeSettings &fes, FeWindow &wnd, FeMusic &ambient_sound, bool console_input );
 	~FeVM();
+
+	void load_script_nv();
+	void save_script_nv();
+	void load_layout_nv();
+	void save_layout_nv();
 
 	void set_overlay( FeOverlay *feo );
 

src/main.cpp
@@ -196,6 +196,8 @@ int main(int argc, char *argv[])
 	sf::Clock move_timer;
 	std::optional<sf::Event> move_event;
 	int move_last_triggered( 0 );
+	sf::Clock command_timer;
+	command_timer.stop();
 
 	// go straight into config mode if there are no lists configured for
 	// display
@@ -462,6 +464,7 @@ int main(int argc, char *argv[])
 				move_timer.restart();
 				move_event = ev;
 				move_triggered = FeInputMap::LAST_COMMAND;
+				command_timer.restart();
 			}
 
 			//
@@ -1031,6 +1034,7 @@ int main(int argc, char *argv[])
 
 			if ( cont )
 			{
+				command_timer.restart();
 				int t = move_timer.getElapsedTime().asMilliseconds();
 				if (( t > feSettings.selection_delay() ) && ( t - move_last_triggered > feSettings.selection_speed() ))
 				{
@@ -1168,6 +1172,14 @@ int main(int argc, char *argv[])
 			continue;
 		}
 
+		// Save nv a short time after the last ui command
+		if ( command_timer.isRunning() && command_timer.getElapsedTime().asMilliseconds() > 5000 )
+		{
+			command_timer.reset();
+			feVM.save_script_nv();
+			feVM.save_layout_nv();
+		}
+
 		//
 		// Log any unexpected loss of window focus
 		//
@@ -1186,7 +1198,10 @@ int main(int argc, char *argv[])
 			redraw=true;
 
 		if ( feVM.saver_activation_check() )
+		{
 			soundsys.sound_event( FeInputMap::ScreenSaver );
+			command_timer.restart();
+		}
 
 		if ( redraw || !feSettings.get_info_bool( FeSettings::PowerSaving ) )
 		{

commit 0a01b6d3b9199498b0ce5d58b6c503dcfe13adb2

Fix crashes in Displays Menu
Regression caused by 73e7f0a1d3722b8acde4cef665a9a0170246c2f8

src/fe_vm.cpp
@@ -289,8 +289,12 @@ void FeVM::save_script_nv() {
 
 // Load the fe.layout.nv for the current layout
 void FeVM::load_layout_nv() {
-	FeDisplayInfo *di = m_feSettings->get_display( m_feSettings->get_current_display_index() );
-	m_last_layout = di->get_layout();
+	int idx = m_feSettings->get_current_display_index();
+
+	if ( idx < 0 )
+		m_last_layout = m_feSettings->get_info( FeSettings::MenuLayout );
+	else
+		m_last_layout = m_feSettings->get_display( idx )->get_layout();
 
 	std::string layout_nv;
 	read_file_content( m_feSettings->get_config_dir() + FE_LAYOUT_NV_FILE, layout_nv );