oomek/attractplus

commit 2310ead5d870491fd739c8850dcc61908118398e

Audio fixes and improvements
- Fix videos with mono audio playing only in left channel
- Disable audio spatialization by default
- Enable audio spatialization only when setPosition() is used
- Fix volume discrepancies between mono and stereo files
- Remove vu * 2 multiplier workaround
- Move volume logic to tick() of each class
- Fix FeSound and FeAudio volume being overridden by volume in Settings
- Add pan property to FeImage, FeSound and FeMusic

src/fe_music.cpp
@@ -28,6 +28,7 @@
 #include "fe_util.hpp"
 #include "fe_file.hpp"
 
+#include <algorithm>
 #include <fstream>
 #include <unordered_map>
 #include <cmath>
@@ -42,28 +43,20 @@
 FeMusic::FeMusic( bool loop )
 	: m_file_name( "" ),
 	m_volume( 100.0 ),
+	m_pan( 0.0 ),
 	m_fft_data_zero( FeAudioVisualiser::FFT_BANDS_MAX, 0.0f ),
 	m_fft_zero_wrapper( &m_fft_data_zero ),
 	m_fft_array_wrapper( &m_fft_data_zero )
 {
 	m_music.setLooping( loop );
+	m_music.setSpatializationEnabled( false );
+
 	m_audio_effects.add_effect( std::make_unique<FeAudioDCFilter>() );
 	m_audio_effects.add_effect( std::make_unique<FeAudioNormaliser>() );
 	m_audio_effects.add_effect( std::make_unique<FeAudioVisualiser>() );
 
 	// Mark effects manager as ready for processing after all effects are constructed
 	m_audio_effects.set_ready_for_processing();
-
-	FePresent *fep = FePresent::script_get_fep();
-	if ( fep )
-	{
-		float volume = fep->get_fes()->get_play_volume( FeSoundInfo::Sound );
-		m_music.setVolume( volume );
-
-		auto* normaliser = m_audio_effects.get_effect<FeAudioNormaliser>();
-		if ( normaliser )
-			normaliser->set_media_volume( volume / 100.0f );
-	}
 	m_music.setEffectProcessor( [this]( const float *input_frames, unsigned int &input_frame_count,
 	                                    float *output_frames, unsigned int &output_frame_count,
 	                                    unsigned int frame_channel_count )
@@ -149,17 +142,18 @@ void FeMusic::set_volume( float v )
 		else if ( v < 0.0 ) v = 0.0;
 
 		m_volume = v;
-
-		FePresent *fep = FePresent::script_get_fep();
-		if ( fep )
-			v = v * fep->get_fes()->get_play_volume( FeSoundInfo::Sound ) / 100.0;
-
-		m_music.setVolume( v );
-
-		auto* normaliser = m_audio_effects.get_effect<FeAudioNormaliser>();
-		if ( normaliser )
-			normaliser->set_media_volume( v / 100.0f );
 	}
+}
+
+float FeMusic::get_pan()
+{
+	return m_pan;
+}
+
+void FeMusic::set_pan( float p )
+{
+	if ( p == m_pan ) return;
+	m_pan = std::clamp( p, -1.0f, 1.0f );
 }
 
 bool FeMusic::get_playing()
@@ -213,16 +207,19 @@ float FeMusic::get_z()
 void FeMusic::set_x( float v )
 {
 	m_music.setPosition( sf::Vector3f( v, get_y(), get_z() ) );
+	m_music.setSpatializationEnabled( true );
 }
 
 void FeMusic::set_y( float v )
 {
 	m_music.setPosition( sf::Vector3f( get_x(), v, get_z() ) );
+	m_music.setSpatializationEnabled( true );
 }
 
 void FeMusic::set_z( float v )
 {
 	m_music.setPosition( sf::Vector3f( get_x(), get_y(), v ) );
+	m_music.setSpatializationEnabled( true );
 }
 
 int FeMusic::get_duration()
@@ -272,6 +269,24 @@ const char *FeMusic::get_metadata( const char* tag )
 
 void FeMusic::tick()
 {
+	float vol = m_volume;
+
+	FePresent *fep = FePresent::script_get_fep();
+	if ( fep )
+		vol = vol * fep->get_fes()->get_play_volume( FeSoundInfo::Sound ) / 100.0;
+
+	if ( vol != m_music.getVolume() )
+	{
+		m_music.setVolume( vol );
+
+		auto* normaliser = m_audio_effects.get_effect<FeAudioNormaliser>();
+		if ( normaliser )
+			normaliser->set_media_volume( vol / 100.0f );
+	}
+
+	if ( m_pan != m_music.getPan() )
+		m_music.setPan( m_pan );
+
 	m_audio_effects.update_all();
 }
 

src/fe_music.hpp
@@ -39,6 +39,7 @@ private:
 	std::string m_file_name;
 	bool m_play_state;
 	float m_volume;
+	float m_pan;
 
 	FeAudioEffectsManager m_audio_effects;
 
@@ -53,6 +54,9 @@ public:
 
 	float get_volume();
 	void set_volume( float );
+
+	float get_pan();
+	void set_pan( float );
 
 	bool get_playing();
 	void set_playing( bool );

src/fe_sound.cpp
@@ -26,6 +26,8 @@
 #include "fe_util.hpp"
 #include "fe_file.hpp"
 #include "zip.hpp"
+
+#include <algorithm>
 #include <iostream>
 #include <cstring>
 
@@ -91,12 +93,6 @@ void FeSoundSystem::tick()
 {
 }
 
-void FeSoundSystem::update_volumes()
-{
-	m_ambient_sound.set_volume( m_fes->get_play_volume( FeSoundInfo::Ambient ) );
-	m_event_sound.set_volume( m_fes->get_play_volume( FeSoundInfo::Sound ) );
-}
-
 void FeSoundSystem::release_audio( bool state )
 {
 	m_event_sound.release_audio( state );
@@ -108,10 +104,12 @@ FeSound::FeSound( bool loop )
 	m_file_name( "" ),
 	m_play_state( false ),
 	m_volume( 100.0 ),
+	m_pan( 0.0 ),
 	m_pitch( 1.0 ),
 	m_loop( loop ),
 	m_position( 0.0, 0.0, 0.0 )
 {
+	m_sound.setSpatializationEnabled( false );
 }
 
 FeSound::~FeSound()
@@ -127,6 +125,17 @@ void FeSound::release_audio( bool state )
 
 void FeSound::tick()
 {
+	float vol = m_volume;
+
+	FePresent *fep = FePresent::script_get_fep();
+	if ( fep )
+		vol = vol * fep->get_fes()->get_play_volume( FeSoundInfo::Sound ) / 100.0;
+
+	if ( vol != m_sound.getVolume() )
+		m_sound.setVolume( vol );
+
+	if ( m_pan != m_sound.getPan() )
+		m_sound.setPan( m_pan );
 }
 
 void FeSound::load( const std::string &fn )
@@ -163,13 +172,18 @@ void FeSound::set_volume( float v )
 		else if ( v < 0.0 ) v = 0.0;
 
 		m_volume = v;
-
-		FePresent *fep = FePresent::script_get_fep();
-		if ( fep )
-			v = v * fep->get_fes()->get_play_volume( FeSoundInfo::Sound ) / 100.0;
-
-		m_sound.setVolume( v );
 	}
+}
+
+float FeSound::get_pan()
+{
+	return m_pan;
+}
+
+void FeSound::set_pan( float p )
+{
+	if ( p == m_pan ) return;
+	m_pan = std::clamp( p, -1.0f, 1.0f );
 }
 
 void FeSound::set_playing( bool flag )
@@ -179,11 +193,6 @@ void FeSound::set_playing( bool flag )
 	if ( m_play_state == true && m_file_name != "" )
 	{
 		m_sound.stop();
-		FePresent *fep = FePresent::script_get_fep();
-		float vol = m_volume;
-		if ( fep )
-			vol = vol * fep->get_fes()->get_play_volume( FeSoundInfo::Sound ) / 100.0;
-		m_sound.setVolume( vol );
 		m_sound.setLooping( m_loop );
 		m_sound.setPosition( m_position );
 		m_sound.setPitch( m_pitch );
@@ -252,6 +261,7 @@ void FeSound::set_x( float p )
 		m_position.x = p;
 
 		m_sound.setPosition( m_position );
+		m_sound.setSpatializationEnabled( true );
 	}
 }
 
@@ -262,6 +272,7 @@ void FeSound::set_y( float p )
 		m_position.y = p;
 
 		m_sound.setPosition( m_position );
+		m_sound.setSpatializationEnabled( true );
 	}
 }
 
@@ -272,6 +283,7 @@ void FeSound::set_z( float p )
 		m_position.z = p;
 
 		m_sound.setPosition( m_position );
+		m_sound.setSpatializationEnabled( true );
 	}
 }
 

src/fe_audio_fx.cpp
@@ -271,10 +271,9 @@ bool FeAudioVisualiser::process( const float* input_frames, float* output_frames
 		}
 
 		// Hold the maximum peak until fall processing
-		// Multiply by 2.0 to scale from SFML's -0.5 to 0.5 range
-		float new_mono = convert_to_log_scale( peak_mono * 2.0f, VU_AMPLITUDE_LINEARITY );
-		float new_left = convert_to_log_scale( peak_left * 2.0f, VU_AMPLITUDE_LINEARITY );
-		float new_right = convert_to_log_scale( peak_right * 2.0f, VU_AMPLITUDE_LINEARITY );
+		float new_mono = convert_to_log_scale( peak_mono, VU_AMPLITUDE_LINEARITY );
+		float new_left = convert_to_log_scale( peak_left, VU_AMPLITUDE_LINEARITY );
+		float new_right = convert_to_log_scale( peak_right, VU_AMPLITUDE_LINEARITY );
 
 		if ( new_mono > m_vu_mono_in ) m_vu_mono_in = new_mono;
 		if ( new_left > m_vu_left_in ) m_vu_left_in = new_left;
@@ -732,7 +731,7 @@ bool FeAudioNormaliser::process( const float *input_frames, float *output_frames
 		return true;
 	}
 
-	const float target_level = 0.5f * media_volume;
+	const float target_level = media_volume;
 
 	if ( current_peak > max_peak )
 		max_peak = current_peak;

src/fe_image.cpp
@@ -30,6 +30,8 @@
 #include "fe_audio_fx.hpp"
 #include "zip.hpp"
 #include "image_loader.hpp"
+
+#include <algorithm>
 #include <cmath>
 
 #ifndef NO_MOVIE
@@ -56,10 +58,6 @@ void FeBaseTextureContainer::set_play_state( bool play )
 bool FeBaseTextureContainer::get_play_state() const
 {
 	return false;
-}
-
-void FeBaseTextureContainer::set_vol( float vol )
-{
 }
 
 void FeBaseTextureContainer::set_index_offset( int io, bool do_update )
@@ -162,6 +160,15 @@ float FeBaseTextureContainer::get_volume() const
 	return 0.0;
 }
 
+void FeBaseTextureContainer::set_pan( float p )
+{
+}
+
+float FeBaseTextureContainer::get_pan() const
+{
+	return 0.0;
+}
+
 float FeBaseTextureContainer::get_sample_aspect_ratio() const
 {
 	return 1.0;
@@ -252,6 +259,7 @@ FeTextureContainer::FeTextureContainer(
 	m_mipmap( false ),
 	m_smooth( false ),
 	m_volume( 100.0 ),
+	m_pan( 0.0 ),
 	m_fft_bands( 32 ),
 	m_entry( NULL )
 {
@@ -582,6 +590,22 @@ bool FeTextureContainer::tick( FeSettings *feSettings, bool play_movies )
 #ifndef NO_MOVIE
 	if ( m_movie )
 	{
+		float vol = m_volume * feSettings->get_play_volume( FeSoundInfo::Movie ) / 100.0;
+
+		if ( m_video_flags & VF_NoAudio )
+		{
+			if ( vol != m_movie->getVolume() )
+				m_movie->setVolume( 0.f );
+		}
+		else
+		{
+			if ( vol != m_movie->getVolume() )
+				m_movie->setVolume( vol );
+
+			if ( m_pan != m_movie->getPan() )
+				m_movie->setPan( m_pan );
+		}
+
 		if ( m_movie_status > 0 )
 		{
 			if ( m_movie_status < PLAY_COUNT )
@@ -598,15 +622,6 @@ bool FeTextureContainer::tick( FeSettings *feSettings, bool play_movies )
 			else if ( m_movie_status == PLAY_COUNT )
 			{
 				m_movie_status++;
-
-				//
-				// Start playing now if this is a video...
-				//
-				if ( m_video_flags & VF_NoAudio )
-					m_movie->setVolume( 0.f );
-				else
-					m_movie->setVolume( m_volume * feSettings->get_play_volume( FeSoundInfo::Movie ) / 100.0 );
-
 				m_movie->play();
 			}
 
@@ -682,14 +697,6 @@ bool FeTextureContainer::get_play_state() const
 	return false;
 }
 
-void FeTextureContainer::set_vol( float vol )
-{
-#ifndef NO_MOVIE
-	if ( (m_movie) && !(m_video_flags & VF_NoAudio) )
-		m_movie->setVolume( m_volume * vol / 100.0 );
-#endif
-}
-
 void FeTextureContainer::set_index_offset( int io, bool do_update )
 {
 	if ( m_index_offset != io || m_file_name.empty() )
@@ -729,15 +736,6 @@ void FeTextureContainer::set_video_flags( FeVideoFlags f )
 #ifndef NO_MOVIE
 	if ( m_movie )
 	{
-		if (m_video_flags & VF_NoAudio)
-			m_movie->setVolume( 0.f );
-		else
-		{
-			FePresent *fep = FePresent::script_get_fep();
-			if ( fep )
-				m_movie->setVolume( m_volume * fep->get_fes()->get_play_volume( FeSoundInfo::Movie ) / 100.0 );
-		}
-
 		if ( m_movie_status > 0 && m_video_flags & VF_NoAutoStart )
 			m_movie_status = 0;
 	}
@@ -886,12 +884,22 @@ void FeTextureContainer::set_volume( float v )
 	if ( v < 0.0 ) v = 0.0;
 	if ( v > 100.0 ) v = 100.0;
 	m_volume = v;
-	set_vol( v );
 }
 
 float FeTextureContainer::get_volume() const
 {
 	return m_volume;
+}
+
+void FeTextureContainer::set_pan( float p )
+{
+	if ( p == m_pan ) return;
+	m_pan = std::clamp( p, -1.0f, 1.0f );
+}
+
+float FeTextureContainer::get_pan() const
+{
+	return m_pan;
 }
 
 void FeTextureContainer::set_fft_bands( int count )
@@ -1856,6 +1864,16 @@ void FeImage::set_volume( float v )
 float FeImage::get_volume() const
 {
 	return m_tex->get_volume();
+}
+
+void FeImage::set_pan( float p )
+{
+	m_tex->set_pan( p );
+}
+
+float FeImage::get_pan() const
+{
+	return m_tex->get_pan();
 }
 
 float FeImage::get_vu_mono() const

src/fe_image.hpp
@@ -65,7 +65,6 @@ public:
 
 	virtual void set_play_state( bool play );
 	virtual bool get_play_state() const;
-	virtual void set_vol( float vol );
 
 	virtual void set_index_offset( int io, bool do_update=true );
 	virtual int get_index_offset() const;
@@ -102,6 +101,9 @@ public:
 
 	virtual void set_volume( float );
 	virtual float get_volume() const;
+
+	virtual void set_pan( float );
+	virtual float get_pan() const;
 
 	virtual float get_sample_aspect_ratio() const;
 
@@ -150,7 +152,6 @@ public:
 	bool tick( FeSettings *feSettings, bool play_movies ); // returns true if redraw required
 	void set_play_state( bool play );
 	bool get_play_state() const;
-	void set_vol( float vol );
 
 	void set_index_offset( int io, bool do_update );
 	int get_index_offset() const;
@@ -184,6 +185,9 @@ public:
 
 	void set_volume( float );
 	float get_volume() const;
+
+	void set_pan( float );
+	float get_pan() const;
 
 	void set_fft_bands( int );
 	int get_fft_bands() const;
@@ -228,6 +232,7 @@ private:
 	bool m_mipmap;
 	bool m_smooth;
 	float m_volume;
+	float m_pan;
 	int m_fft_bands;
 	FeImageLoaderEntry *m_entry;
 };
@@ -364,6 +369,7 @@ public:
 	bool get_repeat() const;
 	bool get_redraw() const;
 	float get_volume() const;
+	float get_pan() const;
 	float get_border_scale() const;
 
 	float get_vu_mono() const;
@@ -404,6 +410,7 @@ public:
 	void set_repeat( bool );
 	void set_redraw( bool );
 	void set_volume( float );
+	void set_pan( float );
 	void set_blend_mode( int b );
 	void set_border_scale( float s );
 

src/fe_present.cpp
@@ -701,9 +701,6 @@ FeSound *FePresent::add_sound( const char *n )
 	if ( !name.empty() )
 		new_sound->load( path + name );
 
-	new_sound->set_volume(
-		m_feSettings->get_play_volume( FeSoundInfo::Sound ) );
-
 	m_sounds.push_back( new_sound );
 	return new_sound;
 }
@@ -728,9 +725,6 @@ FeMusic *FePresent::add_music( const char *n )
 
 	if ( !name.empty() )
 		new_music->load( path + name );
-
-	new_music->set_volume(
-		m_feSettings->get_play_volume( FeSoundInfo::Sound ) );
 
 	m_musics.push_back( new_music );
 	return new_music;
@@ -1450,13 +1444,6 @@ void FePresent::post_run()
 		(*its)->release_audio( false );
 #endif
 
-	for ( std::vector<FeBaseTextureContainer *>::iterator itm=m_texturePool.begin();
-				itm != m_texturePool.end(); ++itm )
-		(*itm)->set_vol( m_feSettings->get_play_volume( FeSoundInfo::Movie ) );
-
-	for ( its=m_sounds.begin(); its != m_sounds.end(); ++its )
-		(*its)->set_volume( m_feSettings->get_play_volume( FeSoundInfo::Sound ) );
-
 	set_video_play_state( m_playMovies );
 
 	m_layout_time.tick();
@@ -1482,24 +1469,6 @@ void FePresent::set_video_play_state( bool state )
 	for ( std::vector<FeBaseTextureContainer *>::iterator itm=m_texturePool.begin();
 				itm != m_texturePool.end(); ++itm )
 		(*itm)->set_play_state( state );
-}
-
-void FePresent::toggle_mute()
-{
-	int movie_vol = m_feSettings->get_play_volume( FeSoundInfo::Movie );
-	int sound_vol = m_feSettings->get_play_volume( FeSoundInfo::Sound );
-
-	for ( std::vector<FeBaseTextureContainer *>::iterator itm=m_texturePool.begin();
-				itm != m_texturePool.end(); ++itm )
-		(*itm)->set_vol( movie_vol );
-
-	for ( std::vector<FeSound *>::iterator its=m_sounds.begin();
-				its != m_sounds.end(); ++its )
-		(*its)->set_volume( sound_vol );
-
-	for ( std::vector<FeMusic *>::iterator its=m_musics.begin();
-				its != m_musics.end(); ++its )
-		(*its)->set_volume( sound_vol );
 }
 
 const sf::Transform &FePresent::get_transform() const

src/fe_present.hpp
@@ -255,7 +255,6 @@ public:
 	void on_stop_frontend();
 	void pre_run();
 	void post_run();
-	void toggle_mute();
 
 	bool reset_screen_saver();
 	bool handle_event( FeInputMap::Command );

commit 985f3f496f4b18b9e6b97d55b33d399fb226677d

Fix fe.ambient_sound.volume
Regression caused by 2310ead5d870491fd739c8850dcc61908118398e

src/fe_music.cpp
@@ -163,6 +163,11 @@ bool FeMusic::get_playing()
 
 void FeMusic::set_playing( bool state )
 {
+	set_playing( state, FeSoundInfo::Sound );
+}
+
+void FeMusic::set_playing( bool state, FeSoundInfo::SoundType st )
+{
 	m_music.stop();
 
 	if ( state == true && m_file_name != "" )
@@ -170,7 +175,7 @@ void FeMusic::set_playing( bool state )
 		float vol = m_volume;
 		FePresent *fep = FePresent::script_get_fep();
 		if ( fep )
-			vol = vol * fep->get_fes()->get_play_volume( FeSoundInfo::Sound ) / 100.0;
+			vol = vol * fep->get_fes()->get_play_volume( st ) / 100.0;
 
 		m_music.setVolume( vol );
 		m_music.setPan( m_pan );
@@ -281,13 +286,13 @@ const char *FeMusic::get_metadata( const char* tag )
 	return it != metadata.end() ? it->second.c_str() : "";
 }
 
-void FeMusic::tick()
+void FeMusic::tick( FeSoundInfo::SoundType st )
 {
 	float vol = m_volume;
 
 	FePresent *fep = FePresent::script_get_fep();
 	if ( fep )
-		vol = vol * fep->get_fes()->get_play_volume( FeSoundInfo::Sound ) / 100.0;
+		vol = vol * fep->get_fes()->get_play_volume( st ) / 100.0;
 
 	if ( vol != m_music.getVolume() )
 	{

src/fe_music.hpp
@@ -28,6 +28,7 @@
 #include <sqrat.h>
 #include "fe_audio_fx.hpp"
 #include "sqrat_array_wrapper.hpp"
+#include "fe_input.hpp"
 
 class FeMusic
 {
@@ -60,6 +61,7 @@ public:
 
 	bool get_playing();
 	void set_playing( bool );
+	void set_playing( bool, FeSoundInfo::SoundType st );
 
     float get_pitch();
 	void set_pitch( float );
@@ -78,7 +80,7 @@ public:
 	int get_time();
 	const char *get_metadata( const char * );
 
-	void tick();
+	void tick( FeSoundInfo::SoundType st = FeSoundInfo::Sound );
 
 	FeAudioVisualiser* get_audio_visualiser() const;
 

src/fe_sound.cpp
@@ -81,16 +81,17 @@ void FeSoundSystem::play_ambient()
 	if ( sound.compare( m_ambient_sound.get_file_name() ) != 0 )
 		m_ambient_sound.load( sound );
 
-	m_ambient_sound.set_playing( true );
+	m_ambient_sound.set_playing( true, FeSoundInfo::Ambient );
 }
 
 void FeSoundSystem::stop()
 {
-	m_ambient_sound.set_playing( false );
+	m_ambient_sound.set_playing( false, FeSoundInfo::Ambient );
 }
 
 void FeSoundSystem::tick()
 {
+	m_ambient_sound.tick( FeSoundInfo::Ambient );
 }
 
 void FeSoundSystem::release_audio( bool state )