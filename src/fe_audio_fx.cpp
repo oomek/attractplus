oomek/attractplus

commit 2d0d40803620ca98c827fb97a4b586292d2537ca

Fix audio normaliser spikes
- Fix audio normaliser being always enabled momentarily when layout takes longer to initialise
- Fix fe.ambient_sound.playing
- Fix audio volume bursts caused by using FeSoundInfo::Sound volume momentarily
- Simplify FeMusic::set_playing and FeMusic::tick functions
- Fix FeMusic issues
- Fix fe.ambient_sound always having normalisation enabled
- Fix normalisation not resetting on media change
- Move audio normaliser logic from tick() to update_all()
- Remove audiofx reset logic from unnecessary places in media.cpp
- Fix wrong initial audio normaliser state of fe.ambient_sound
- Manifests on busy loop / slow layout initialisation
- Add missing ambient sound tick() calls in overlay draw loops
- Fix ambient sound VU and FFT freezes when overlay is open.

src/fe_audio_fx.cpp
@@ -103,15 +103,20 @@ bool FeAudioEffectsManager::process_all( const float *input_frames, float *outpu
 	for ( size_t i = 0; i < m_effects.size(); ++i )
 	{
 		auto& effect = m_effects[i];
-		if ( effect && effect->is_enabled() )
+		if ( effect )
 		{
-			if ( effect->process( current_input, current_output, frame_count, channel_count ))
+			bool enabled = effect->is_enabled();
+			if ( dynamic_cast<FeAudioNormaliser*>( effect.get() ))
+			{
+				FePresent *fep = FePresent::script_get_fep();
+				if ( fep )
+					enabled = fep->get_fes()->get_loudness();
+			}
+
+			if ( enabled && effect->process( current_input, current_output, frame_count, channel_count ))
 				audio_modified = true;
-		}
-		else if ( effect )
-		{
-			// Pass-through
-			std::memcpy( current_output, current_input, frame_count * channel_count * sizeof( float ) );
+			else
+				std::memcpy( current_output, current_input, frame_count * channel_count * sizeof( float ) );
 		}
 
 		std::swap( current_input, current_output );

src/fe_music.hpp
@@ -41,11 +41,12 @@ private:
 	bool m_play_state;
 	float m_volume;
 	float m_pan;
+	FeSoundInfo::SoundType m_sound_type;
 
 	FeAudioEffectsManager m_audio_effects;
 
 public:
-	FeMusic( bool loop=false );
+	FeMusic( bool loop=false, FeSoundInfo::SoundType st=FeSoundInfo::Sound );
 	~FeMusic();
 
 	void load( const std::string &fn );
@@ -61,7 +62,6 @@ public:
 
 	bool get_playing();
 	void set_playing( bool );
-	void set_playing( bool, FeSoundInfo::SoundType st );
 
     float get_pitch();
 	void set_pitch( float );
@@ -80,7 +80,7 @@ public:
 	int get_time();
 	const char *get_metadata( const char * );
 
-	void tick( FeSoundInfo::SoundType st = FeSoundInfo::Sound );
+	void tick();
 
 	FeAudioVisualiser* get_audio_visualiser() const;
 

src/fe_music.cpp
@@ -40,10 +40,11 @@
 #endif
 
 
-FeMusic::FeMusic( bool loop )
+FeMusic::FeMusic( bool loop, FeSoundInfo::SoundType st )
 	: m_file_name( "" ),
 	m_volume( 100.0 ),
 	m_pan( 0.0 ),
+	m_sound_type( st ),
 	m_fft_data_zero( FeAudioVisualiser::FFT_BANDS_MAX, 0.0f ),
 	m_fft_zero_wrapper( &m_fft_data_zero ),
 	m_fft_array_wrapper( &m_fft_data_zero )
@@ -54,6 +55,14 @@ FeMusic::FeMusic( bool loop )
 	m_audio_effects.add_effect( std::make_unique<FeAudioDCFilter>() );
 	m_audio_effects.add_effect( std::make_unique<FeAudioNormaliser>() );
 	m_audio_effects.add_effect( std::make_unique<FeAudioVisualiser>() );
+
+	FePresent *fep = FePresent::script_get_fep();
+	if ( fep )
+	{
+		auto* normaliser = m_audio_effects.get_effect<FeAudioNormaliser>();
+		if ( normaliser )
+			normaliser->set_enabled( fep->get_fes()->get_loudness() );
+	}
 
 	// Mark effects manager as ready for processing after all effects are constructed
 	m_audio_effects.set_ready_for_processing();
@@ -105,6 +114,10 @@ void FeMusic::load( const std::string &fn )
 		return;
 	}
 	m_file_name = fn;
+
+	auto* normaliser = m_audio_effects.get_effect<FeAudioNormaliser>();
+	if ( normaliser )
+		normaliser->reset();
 }
 
 void FeMusic::set_file_name( const char *n )
@@ -163,11 +176,6 @@ bool FeMusic::get_playing()
 
 void FeMusic::set_playing( bool state )
 {
-	set_playing( state, FeSoundInfo::Sound );
-}
-
-void FeMusic::set_playing( bool state, FeSoundInfo::SoundType st )
-{
 	m_music.stop();
 
 	if ( state == true && m_file_name != "" )
@@ -175,7 +183,7 @@ void FeMusic::set_playing( bool state, FeSoundInfo::SoundType st )
 		float vol = m_volume;
 		FePresent *fep = FePresent::script_get_fep();
 		if ( fep )
-			vol = vol * fep->get_fes()->get_play_volume( st ) / 100.0;
+			vol = vol * fep->get_fes()->get_play_volume( m_sound_type ) / 100.0;
 
 		m_music.setVolume( vol );
 		m_music.setPan( m_pan );
@@ -286,13 +294,13 @@ const char *FeMusic::get_metadata( const char* tag )
 	return it != metadata.end() ? it->second.c_str() : "";
 }
 
-void FeMusic::tick( FeSoundInfo::SoundType st )
+void FeMusic::tick()
 {
 	float vol = m_volume;
 
 	FePresent *fep = FePresent::script_get_fep();
 	if ( fep )
-		vol = vol * fep->get_fes()->get_play_volume( st ) / 100.0;
+		vol = vol * fep->get_fes()->get_play_volume( m_sound_type ) / 100.0;
 
 	if ( vol != m_music.getVolume() )
 	{

src/fe_overlay.cpp
@@ -300,10 +300,12 @@ private:
 
 FeOverlay::FeOverlay( FeWindow &wnd,
 		FeSettings &fes,
-		FePresent &fep )
+		FePresent &fep,
+		FeSoundSystem &ss )
 	: m_wnd( wnd ),
 	m_feSettings( fes ),
 	m_fePresent( fep ),
+	m_soundSystem( ss ),
 	m_overlay_is_on( false )
 {
 	clear_menu_command();
@@ -400,6 +402,8 @@ void FeOverlay::splash_message( const std::string &msg, const std::string &aux )
 	if ( m_fePresent.is_layout_loaded() )
 		m_fePresent.tick();
 
+	m_soundSystem.tick();
+
 	m_wnd.clear();
 	m_wnd.draw( m_fePresent, t );
 	m_wnd.draw( bg, t );
@@ -423,6 +427,8 @@ void FeOverlay::splash_logo( const std::string &aux )
 	// Process tick only when Layout is fully loaded
 	if ( m_fePresent.is_layout_loaded() )
 		m_fePresent.tick();
+
+	m_soundSystem.tick();
 
 	m_wnd.clear();
 	m_wnd.draw( m_fePresent, t );
@@ -969,6 +975,8 @@ void FeOverlay::input_map_dialog(
 
 		if ( m_fePresent.tick() )
 			redraw = true;
+
+		m_soundSystem.tick();
 
 		if ( redraw || !m_feSettings.get_info_bool( FeSettings::PowerSaving ) )
 		{
@@ -1835,6 +1843,8 @@ void FeOverlay::init_event_loop( FeEventLoopCtx &ctx )
 
 		if ( m_fePresent.tick() )
 		{
+			m_soundSystem.tick();
+
 			m_fePresent.redraw_surfaces();
 			m_wnd.clear();
 			m_wnd.draw( m_fePresent, t );
@@ -1964,6 +1974,8 @@ bool FeOverlay::event_loop( FeEventLoopCtx &ctx )
 
 		if ( m_fePresent.tick() )
 			redraw = true;
+
+		m_soundSystem.tick();
 
 		if ( redraw || !m_feSettings.get_info_bool( FeSettings::PowerSaving ) )
 		{
@@ -2329,6 +2341,8 @@ bool FeOverlay::edit_loop( std::vector<sf::Drawable *> d,
 
 		if ( m_fePresent.tick() )
 			redraw = true;
+
+		m_soundSystem.tick();
 
 		// When left or right is hold reset the timer, so the cursor isn't blinking
 		if ( m_feSettings.get_current_state( FeInputMap::Left ) || m_feSettings.get_current_state( FeInputMap::Right ))

src/fe_overlay.hpp
@@ -45,6 +45,7 @@ private:
 	FeWindow &m_wnd;
 	FeSettings &m_feSettings;
 	FePresent &m_fePresent;
+	FeSoundSystem &m_soundSystem;
 
 	sf::Color m_bg_color;
 	sf::Color m_text_color;
@@ -160,7 +161,8 @@ public:
 
 	FeOverlay( FeWindow &wnd,
 		FeSettings &fes,
-		FePresent &fep );
+		FePresent &fep,
+		FeSoundSystem &ss );
 
 	void splash_message( const std::string &, const std::string &aux="" );
 	void splash_logo( const std::string &aux="" );

src/fe_sound.cpp
@@ -33,7 +33,7 @@
 
 FeSoundSystem::FeSoundSystem( FeSettings *fes )
 	: m_event_sound( false ),
-	m_ambient_sound( true ),
+	m_ambient_sound( true, FeSoundInfo::Ambient ),
 	m_fes( fes ),
 	m_current_sound( FeInputMap::LAST_COMMAND )
 {
@@ -81,17 +81,17 @@ void FeSoundSystem::play_ambient()
 	if ( sound.compare( m_ambient_sound.get_file_name() ) != 0 )
 		m_ambient_sound.load( sound );
 
-	m_ambient_sound.set_playing( true, FeSoundInfo::Ambient );
+	m_ambient_sound.set_playing( true );
 }
 
 void FeSoundSystem::stop()
 {
-	m_ambient_sound.set_playing( false, FeSoundInfo::Ambient );
+	m_ambient_sound.set_playing( false );
 }
 
 void FeSoundSystem::tick()
 {
-	m_ambient_sound.tick( FeSoundInfo::Ambient );
+	m_ambient_sound.tick();
 }
 
 void FeSoundSystem::release_audio( bool state )

src/main.cpp
@@ -171,7 +171,7 @@ int main(int argc, char *argv[])
 #endif
 
 	FeVM feVM( feSettings, window, soundsys.get_ambient_sound(), process_console );
-	FeOverlay feOverlay( window, feSettings, feVM );
+	FeOverlay feOverlay( window, feSettings, feVM, soundsys );
 	feVM.set_overlay( &feOverlay );
 
 	// Set transforms now in case load_layout never gets called (ie: first-run), required for multimon

src/media.cpp
@@ -935,6 +935,14 @@ FeMedia::FeMedia( Type t )
 	m_audio_effects.add_effect( std::make_unique<FeAudioNormaliser>() );
 	m_audio_effects.add_effect( std::make_unique<FeAudioVisualiser>() );
 
+	FePresent *fep = FePresent::script_get_fep();
+	if ( fep )
+	{
+		auto* normaliser = m_audio_effects.get_effect<FeAudioNormaliser>();
+		if ( normaliser )
+			normaliser->set_enabled( fep->get_fes()->get_loudness() );
+	}
+
 	// Mark effects manager as ready for processing after all effects are constructed
 	m_audio_effects.set_ready_for_processing();
 
@@ -968,15 +976,10 @@ void FeMedia::setup_effect_processor()
 		{
 			m_audio_effects.process_all( input_frames, output_frames, input_frame_count, frame_channel_count );
 		}
-		else
+		else if ( input_frames && output_frames && input_frame_count > 0 )
 		{
-			m_audio_effects.reset_all();
-
-			if ( input_frames && output_frames && input_frame_count > 0 )
-			{
-				const unsigned int total_samples = input_frame_count * frame_channel_count;
-				std::memcpy( output_frames, input_frames, total_samples * sizeof( float ));
-			}
+			const unsigned int total_samples = input_frame_count * frame_channel_count;
+			std::memcpy( output_frames, input_frames, total_samples * sizeof( float ));
 		}
 
 		output_frame_count = input_frame_count;
@@ -1001,8 +1004,6 @@ void FeMedia::play()
 {
 	if ( !is_playing() )
 	{
-		m_audio_effects.reset_all();
-
 		if ( m_video )
 			m_video->play();
 
@@ -1018,8 +1019,6 @@ void FeMedia::signal_stop()
 
 	if ( m_video )
 		m_video->signal_stop();
-
-	m_audio_effects.reset_all();
 }
 
 void FeMedia::stop()
@@ -1061,8 +1060,6 @@ void FeMedia::stop()
 		std::lock_guard<std::recursive_mutex> l( m_imp->m_read_mutex );
 		m_imp->m_read_eof = false;
 	}
-
-	m_audio_effects.reset_all();
 }
 
 void FeMedia::close()
@@ -1180,8 +1177,6 @@ size_t fe_media_seek( void *opaque, int64_t offset, int whence )
 bool FeMedia::open( const std::string &archive,
 	const std::string &name, sf::Texture *outt )
 {
-	m_audio_effects.reset_all();
-
 	FeFileInputStream *s = NULL;
 
 	if ( !archive.empty() )

commit aa811d7b9c69590ed4bcbd6b3b8b01709bea6d94

Fix use after free condition in audio normaliser
- Regression caused by: 2d0d40803620ca98c827fb97a4b586292d2537ca
- Rename FeSoundInfo m_sounds to m_sound_info

src/fe_audio_fx.cpp
@@ -106,12 +106,6 @@ bool FeAudioEffectsManager::process_all( const float *input_frames, float *outpu
 		if ( effect )
 		{
 			bool enabled = effect->is_enabled();
-			if ( dynamic_cast<FeAudioNormaliser*>( effect.get() ))
-			{
-				FePresent *fep = FePresent::script_get_fep();
-				if ( fep )
-					enabled = fep->get_fes()->get_loudness();
-			}
 
 			if ( enabled && effect->process( current_input, current_output, frame_count, channel_count ))
 				audio_modified = true;

src/fe_audio_fx.hpp
@@ -54,6 +54,7 @@ protected:
 	float m_device_sample_rate;
 };
 
+class FeAudioNormaliser;
 
 class FeAudioEffectsManager
 {
@@ -68,6 +69,8 @@ public:
 	void update_all();
 	void reset_all();
 	void set_ready_for_processing();
+
+	FeAudioNormaliser* get_normaliser() const;
 
 	// Get specific effect (returns nullptr if not found or wrong type)
 	template<typename T>
@@ -199,6 +202,11 @@ private:
 	float m_media_volume = 1.0f;
 	float m_max_peak = 0.0f;
 };
+
+inline FeAudioNormaliser* FeAudioEffectsManager::get_normaliser() const
+{
+	return get_effect<FeAudioNormaliser>();
+}
 
 template<typename T>
 T* FeAudioEffectsManager::get_effect() const

src/fe_image.hpp
@@ -202,6 +202,7 @@ public:
 
 	float get_sample_aspect_ratio() const;
 
+	FeAudioEffectsManager& get_audio_effects() { return m_audio_effects; };
 	FeMedia *get_media() const;
 
 protected:

src/fe_music.hpp
@@ -82,6 +82,7 @@ public:
 
 	void tick();
 
+	FeAudioEffectsManager& get_audio_effects() { return m_audio_effects; };
 	FeAudioVisualiser* get_audio_visualiser() const;
 
 	float get_vu_mono();

src/fe_present.cpp
@@ -1484,6 +1484,19 @@ void FePresent::set_video_play_state( bool state )
 		(*itm)->set_play_state( state );
 }
 
+void FePresent::set_audio_loudness( bool enabled )
+{
+	for ( FeMusic *music : m_musics )
+		if ( music )
+			if ( auto normaliser = music->get_audio_effects().get_normaliser() )
+				normaliser->set_enabled( enabled );
+
+	for ( FeBaseTextureContainer *container : m_texturePool )
+		if ( auto tex = dynamic_cast<FeTextureContainer *>(container) )
+			if ( auto normaliser = tex->get_audio_effects().get_normaliser() )
+				normaliser->set_enabled( enabled );
+}
+
 const sf::Transform &FePresent::get_transform() const
 {
 	return m_layout_transform;

src/fe_present.hpp
@@ -293,6 +293,7 @@ public:
 	bool get_overlay_custom_controls( FeText *&, FeListBox *& );
 
 	void set_video_play_state( bool state );
+	void set_audio_loudness( bool enabled );
 	bool get_video_toggle() { return m_playMovies; };
 
 	int get_layout_ms();

src/fe_settings.cpp
@@ -655,7 +655,7 @@ int FeSettings::process_setting( const std::string &setting,
 		m_current_config_object = &m_displays.back();
 	}
 	else if ( setting.compare( otherSettingStrings[1] ) == 0 ) // sound
-		m_current_config_object = &m_sounds;
+		m_current_config_object = &m_sound_info;
 	else if ( setting.compare( otherSettingStrings[2] ) == 0 ) // input_map
 		m_current_config_object = &m_inputmap;
 	else if ( setting.compare( otherSettingStrings[3] ) == 0 ) // general
@@ -2640,43 +2640,47 @@ void FeSettings::toggle_layout()
 
 void FeSettings::set_volume( FeSoundInfo::SoundType t, const std::string &v )
 {
-	m_sounds.set_volume( t, v );
+	m_sound_info.set_volume( t, v );
 }
 
 int FeSettings::get_set_volume( FeSoundInfo::SoundType t ) const
 {
-	return m_sounds.get_set_volume( t );
+	return m_sound_info.get_set_volume( t );
 }
 
 int FeSettings::get_play_volume( FeSoundInfo::SoundType t ) const
 {
-	return m_sounds.get_play_volume( t );
+	return m_sound_info.get_play_volume( t );
 }
 
 bool FeSettings::get_mute() const
 {
-	return m_sounds.get_mute();
+	return m_sound_info.get_mute();
 }
 
 void FeSettings::set_mute( bool m )
 {
-	m_sounds.set_mute( m );
+	m_sound_info.set_mute( m );
 }
 
 bool FeSettings::get_loudness() const
 {
-	return m_sounds.get_loudness();
+	return m_sound_info.get_loudness();
 }
 
 void FeSettings::set_loudness( bool enabled )
 {
-	m_sounds.set_loudness( enabled );
+	m_sound_info.set_loudness( enabled );
+
+	FePresent *fep = FePresent::script_get_fep();
+	if ( fep )
+		fep->set_audio_loudness( enabled );
 }
 
 bool FeSettings::get_sound_file( FeInputMap::Command c, std::string &s, bool full_path ) const
 {
 	std::string filename;
-	if ( m_sounds.get_sound( c, filename ) )
+	if ( m_sound_info.get_sound( c, filename ) )
 	{
 		if ( full_path )
 		{
@@ -2696,7 +2700,7 @@ bool FeSettings::get_sound_file( FeInputMap::Command c, std::string &s, bool ful
 
 void FeSettings::set_sound_file( FeInputMap::Command c, const std::string &s )
 {
-	m_sounds.set_sound( c, s );
+	m_sound_info.set_sound( c, s );
 }
 
 void FeSettings::get_sounds_list( std::vector < std::string > &ll ) const
@@ -4040,7 +4044,7 @@ void FeSettings::save() const
 
 		// sound
 		outfile << otherSettingStrings[1] << std::endl;
-		m_sounds.save( outfile );
+		m_sound_info.save( outfile );
 		outfile << std::endl;
 
 		// input_map

src/fe_settings.hpp
@@ -227,7 +227,7 @@ private:
 	FePathCache m_path_cache;
 
 	FeInputMap m_inputmap;
-	FeSoundInfo m_sounds;
+	FeSoundInfo m_sound_info;
 	FeTranslationMap m_translation_map;
 	FeLayoutInfo m_saver_params;
 	FeLayoutInfo m_intro_params;

src/fe_vm.cpp
@@ -3444,3 +3444,12 @@ void FeVM::init_with_default_layout()
 	// Game listbox
 	cb_add_listbox( 0, 0, flw, flh );
 }
+
+void FeVM::set_audio_loudness( bool enabled )
+{
+	// Call base class to update audio normalisers in FeMusic and FeImage
+	FePresent::set_audio_loudness( enabled );
+
+	if ( auto normaliser = m_ambient_sound.get_audio_effects().get_normaliser() )
+		normaliser->set_enabled( enabled );
+}

src/fe_vm.hpp
@@ -142,6 +142,8 @@ public:
 	//
 	bool setup_wizard();
 
+	void set_audio_loudness( bool enabled );
+
 	// Scripting functionality
 	//
 	void vm_close();