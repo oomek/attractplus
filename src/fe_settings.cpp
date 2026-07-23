2dc1c0abe8d1221e587a1db55d4be34be163a6cb
Radek Dutkiewicz <radicdotkey@gmail.com>
2026-07-23 11:29:34 +0100

    Make the UI more responsive
    - React on key press instead of key release of non repeatable keys
    - React on key press instead of key release of the keys not mapped in combos
    - Remove unnecessary debounce logic

--- src/fe_settings.cpp
+++ src/fe_settings.cpp
@@ -1271,7 +1271,7 @@ void FeSettings::load_state()
 
 void FeSettings::reset_input()
 {
-	m_inputmap.clear_tracked_keys();
+	m_inputmap.clear_held_inputs();
 }
 
 FeInputMap::Command FeSettings::map_input( const std::optional<sf::Event> &e )
@@ -1306,6 +1306,11 @@ void FeSettings::set_default_command( FeInputMap::Command c, FeInputMap::Command
 bool FeSettings::get_current_state( FeInputMap::Command c )
 {
 	return m_inputmap.get_current_state( c, m_joy_thresh );
+}
+
+bool FeSettings::is_input_held()
+{
+	return m_inputmap.is_input_held( m_joy_thresh );
 }
 
 bool FeSettings::get_key_state( std::string key )
