b9b6edcf0c9caaf877915557def0d1ac21ee8dbf
Radek Dutkiewicz <oomek@users.noreply.github.com>
2026-07-24 14:50:23 +0100

    BP: Fix infinite layout reload on resizing the window
    Regression caused by commit: b8f1ba73a

--- src/fe_vm.cpp
+++ src/fe_vm.cpp
@@ -412,6 +412,7 @@ void FeVM::post_command( FeInputMap::Command c )
 bool FeVM::poll_command( FeInputMap::Command &c, std::optional<FeEvent> &ev, bool &from_ui )
 {
 	from_ui = false;
+	ev = std::nullopt;
 
 	if ( !m_posted_commands.empty() )
 	{
@@ -441,7 +442,6 @@ bool FeVM::poll_command( FeInputMap::Command &c, std::optional<FeEvent> &ev, boo
 		}
 	}
 
-	ev = std::nullopt; // An empty event
 	return false;
 }
 

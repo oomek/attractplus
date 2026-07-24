oomek/attractplus

commit b8f1ba73a25a4be41ba0736f6aba2808f1f1dd6d

SFML 3.0.0 migration
- Replace deprecated sf::Int64 with size_t
- Remove deprecated setProcessinginterval()
- Rename deprecated setLoop() and getLoop() to setLooping() and isLooping()
- Remove deprecated sf::NonCopyable. Check if there is a need to delete copy constructor manually
- sf::InputStream function return types are now std::optional
- antialiasingLevel renamed to antiAliasingLevel
- sf::Sound constructor now takes m_buffer
- sf::SoundSource::Status::Playing changed to strog enum
- sf::Image requires non empty constructor
- generateMipmap() nodiscard, requires bool return
- sf::Texture::create() changed to resize() that takes Vector2u
- getPixel() takes Vector2u
- Update more strong enums
- Replace remaining deprecates sf::Uint32 with std::uint32_t
- update sf::Rect<T> syntax
- rotation as sf::degrees instead of float - wip
- sf::Transform::translate takes Vector2 now
- Add static_cast<float> to all translate calls that require it
- Change one sf::Int32 to std::int32_t
- sf::Font::loadFromFile renamed to openFromFile
- loadFromMemory renamed to openFromMemory
- update sf::Rect<T> syntax
- Update setPosition() syntax
- Add missing static_cast<float>
- sf::Event implementation in fe_overlay.cpp
- Update sf::Window syntax
- sf::Event implementation in fe_window.cpp
- Getters by reference no longer supported in SFML 3
- sf::Text no longer accepts no parameter constructor
- sf::Text constructor parameter order changed
- Update sf::Shader syntax
- Update syntax in image_loader.cpp
- Update syntax in main.cpp
- Update syntax in media.cpp

src/fe_vm.cpp
@@ -345,20 +345,19 @@ void FeVM::post_command( FeInputMap::Command c )
 	m_posted_commands.push( c );
 }
 
-bool FeVM::poll_command( FeInputMap::Command &c, sf::Event &ev, bool &from_ui )
+bool FeVM::poll_command( FeInputMap::Command &c, std::optional<sf::Event> &ev, bool &from_ui )
 {
-	from_ui=false;
+	from_ui = false;
 
 	if ( !m_posted_commands.empty() )
 	{
-		c = (FeInputMap::Command)m_posted_commands.front();
+		c = ( FeInputMap::Command )m_posted_commands.front();
 		m_posted_commands.pop();
-		ev.type = sf::Event::Count;
-
 		return true;
 	}
-	else if ( m_window.pollEvent( ev ) )
+	else if  ( const auto event = m_window.pollEvent() )
 	{
+		ev = *event;
 		int t = m_layoutTimer.getElapsedTime().asMilliseconds();
 
 		// Debounce to stop multiples when triggered by a key combo
@@ -366,7 +365,7 @@ bool FeVM::poll_command( FeInputMap::Command &c, sf::Event &ev, bool &from_ui )
 		if ( t - m_last_ui_cmd.asMilliseconds() < 30 )
 			return false;
 
-		c = m_feSettings->map_input( ev );
+		c = m_feSettings->map_input( ev.value() );
 
 		if ( c != FeInputMap::LAST_COMMAND )
 			m_last_ui_cmd = m_layoutTimer.getElapsedTime();

Makefile
@@ -83,7 +83,7 @@ LD=objcopy
 B64FLAGS = -w0
 
 CFLAGS += -DSQUSEDOUBLE
-CFLAGS += -std=c++11
+CFLAGS += -std=c++17
 
 ifndef OPTIMIZE
 OPTIMIZE=2
@@ -141,7 +141,6 @@ RES_IMGS_DIR = $(RES_DIR)/images
 
 _DEP =\
 	fe_base.hpp \
-	fe_file.hpp \
 	fe_util.hpp \
 	fe_util_sq.hpp \
 	fe_info.hpp \
@@ -175,7 +174,6 @@ _DEP =\
 
 _OBJ =\
 	fe_base.o \
-	fe_file.o \
 	fe_util.o \
 	fe_util_sq.o \
 	fe_cmdline.o \

src/fe_blend.cpp
@@ -59,19 +59,19 @@ sf::BlendMode FeBlend::get_blend_mode( int blend_mode )
 			return sf::BlendAdd;
 
 		case FeBlend::Subtract:
-			return sf::BlendMode(sf::BlendMode::SrcAlpha, sf::BlendMode::One, sf::BlendMode::ReverseSubtract,
-								 sf::BlendMode::One, sf::BlendMode::One, sf::BlendMode::ReverseSubtract);
+			return sf::BlendMode(sf::BlendMode::Factor::SrcAlpha, sf::BlendMode::Factor::One, sf::BlendMode::Equation::ReverseSubtract,
+								 sf::BlendMode::Factor::One, sf::BlendMode::Factor::One, sf::BlendMode::Equation::ReverseSubtract);
 		case FeBlend::Screen:
-			return sf::BlendMode(sf::BlendMode::One, sf::BlendMode::OneMinusSrcColor);
+			return sf::BlendMode(sf::BlendMode::Factor::One, sf::BlendMode::Factor::OneMinusSrcColor);
 
 		case FeBlend::Multiply:
-			return sf::BlendMode(sf::BlendMode::DstColor, sf::BlendMode::OneMinusSrcAlpha);
+			return sf::BlendMode(sf::BlendMode::Factor::DstColor, sf::BlendMode::Factor::OneMinusSrcAlpha);
 
 		case FeBlend::Overlay:
-			return sf::BlendMode(sf::BlendMode::DstColor, sf::BlendMode::SrcColor);
+			return sf::BlendMode(sf::BlendMode::Factor::DstColor, sf::BlendMode::Factor::SrcColor);
 
 		case FeBlend::Premultiplied:
-			return sf::BlendMode(sf::BlendMode::One, sf::BlendMode::OneMinusSrcAlpha);
+			return sf::BlendMode(sf::BlendMode::Factor::One, sf::BlendMode::Factor::OneMinusSrcAlpha);
 
 		case FeBlend::None:
 			return sf::BlendNone;
@@ -96,21 +96,21 @@ sf::Shader* FeBlend::get_default_shader( int blend_mode )
 			if ( !default_shader_multiplied )
 			{
 				default_shader_multiplied = new sf::Shader();
-				default_shader_multiplied->loadFromMemory( DEFAULT_SHADER_GLSL_MULTIPLIED, sf::Shader::Fragment );
+				bool ret = default_shader_multiplied->loadFromMemory( DEFAULT_SHADER_GLSL_MULTIPLIED, sf::Shader::Type::Fragment );
 			}
 			return default_shader_multiplied;
 		case FeBlend::Overlay:
 			if ( !default_shader_overlay )
 			{
 				default_shader_overlay = new sf::Shader();
-				default_shader_overlay->loadFromMemory( DEFAULT_SHADER_GLSL_OVERLAY, sf::Shader::Fragment );
+				bool ret = default_shader_overlay->loadFromMemory( DEFAULT_SHADER_GLSL_OVERLAY, sf::Shader::Type::Fragment );
 			}
 			return default_shader_overlay;
 		case FeBlend::Premultiplied:
 			if ( !default_shader_premultiplied )
 			{
 				default_shader_premultiplied = new sf::Shader();
-				default_shader_premultiplied->loadFromMemory( DEFAULT_SHADER_GLSL_PREMULTIPLIED, sf::Shader::Fragment );
+				bool ret = default_shader_premultiplied->loadFromMemory( DEFAULT_SHADER_GLSL_PREMULTIPLIED, sf::Shader::Type::Fragment );
 			}
 			return default_shader_premultiplied;
 		default:

src/fe_config.cpp
@@ -2014,7 +2014,7 @@ void FeMiscMenu::get_options( FeConfigContext &ctx )
 	ctx.fe_settings.get_translation( FeSettings::antialiasingDispTokens[ idx ], aamode );
 	std::vector < std::string > aa_modes;
 	i=0;
-	while ( FeSettings::antialiasingDispTokens[i] != 0 && as_int( FeSettings::antialiasingTokens[ i ] ) <= std::min( static_cast<int>( sf::RenderTexture::getMaximumAntialiasingLevel() ), 8 ))
+	while ( FeSettings::antialiasingDispTokens[i] != 0 && as_int( FeSettings::antialiasingTokens[ i ] ) <= std::min( static_cast<int>( sf::RenderTexture::getMaximumAntiAliasingLevel() ), 8 ))
 	{
 		aa_modes.push_back( std::string() );
 		ctx.fe_settings.get_translation( FeSettings::antialiasingDispTokens[ i ], aa_modes.back() );

src/fe_file.cpp
@@ -35,19 +35,19 @@ FeFileInputStream::~FeFileInputStream()
 		fclose( m_file );
 }
 
-sf::Int64 FeFileInputStream::read( void *data, sf::Int64 size )
+std::optional<std::size_t> FeFileInputStream::read( void *data, std::size_t size )
 {
 	if ( m_file )
-		return fread( data, 1, (size_t)size, m_file );
+		return fread( data, 1, (std::size_t)size, m_file );
 
 	return -1;
 }
 
-sf::Int64 FeFileInputStream::seek( sf::Int64 pos )
+std::optional<std::size_t> FeFileInputStream::seek( std::size_t pos )
 {
 	if ( m_file )
 	{
-		if ( fseek( m_file, (size_t)pos, SEEK_SET ) )
+		if ( fseek( m_file, (std::size_t)pos, SEEK_SET ) )
 			return -1;
 
 		return tell();
@@ -56,7 +56,7 @@ sf::Int64 FeFileInputStream::seek( sf::Int64 pos )
 	return -1;
 }
 
-sf::Int64 FeFileInputStream::tell()
+std::optional<std::size_t> FeFileInputStream::tell()
 {
 	if ( m_file )
 		return ftell( m_file );
@@ -64,13 +64,13 @@ sf::Int64 FeFileInputStream::tell()
 	return -1;
 }
 
-sf::Int64 FeFileInputStream::getSize()
+std::optional<std::size_t> FeFileInputStream::getSize()
 {
 	if ( m_file )
 	{
-		sf::Int64 pos = tell();
+		std::size_t pos = tell();
 		fseek( m_file, 0, SEEK_END );
-		sf::Int64 size = tell();
+		std::size_t size = tell();
 		seek( pos );
 		return size;
 	}

src/fe_file.hpp
@@ -33,10 +33,10 @@ public:
 	FeFileInputStream( const std::string &fn );
 	~FeFileInputStream();
 
-	sf::Int64 read( void *data, sf::Int64 size );
-	sf::Int64 seek( sf::Int64 pos );
-	sf::Int64 tell();
-	sf::Int64 getSize();
+	std::optional<std::size_t> read( void *data, std::size_t size );
+	std::optional<std::size_t> seek( std::size_t pos );
+	std::optional<std::size_t> tell();
+	std::optional<std::size_t> getSize();
 
 private:
 	FILE *m_file;

src/fe_image.cpp
@@ -300,7 +300,7 @@ bool FeTextureContainer::fix_masked_image()
 
 	if (( tmp_s.x > 0 ) && ( tmp_s.y > 0 ))
 	{
-		sf::Color p = tmp_img.getPixel( 0, 0 );
+		sf::Color p = tmp_img.getPixel({ 0, 0 });
 		tmp_img.createMaskFromColor( p );
 
 		if ( m_texture.loadFromImage( tmp_img ) )
@@ -363,8 +363,7 @@ bool FeTextureContainer::load_with_ffmpeg(
 	if ( res && !is_image )
 	{
 		// Fill the first video frame with a black colour
-		sf::Image img;
-		img.create(	m_texture.getSize().x, m_texture.getSize().y, sf::Color( 0, 0, 0 ));
+		sf::Image img({ m_texture.getSize().x, m_texture.getSize().y }, sf::Color( 0, 0, 0 ));
 		m_texture.update( img );
 	}
 
@@ -408,13 +407,12 @@ bool FeTextureContainer::try_to_load(
 
 	// resize our texture accordingly
 	if ( m_texture.getSize() != sf::Vector2u( m_entry->get_width(), m_entry->get_height() ) )
-		m_texture.create( m_entry->get_width(), m_entry->get_height() );
 
-	if ( data )
+	if ( data && m_texture.resize({ static_cast<unsigned int>( m_entry->get_width() ), static_cast<unsigned int>( m_entry->get_height() )}))
 	{
 		m_texture.update( data );
 		il.release_entry( &m_entry ); // don't need entry any more
-		if ( m_mipmap ) m_texture.generateMipmap();
+		if ( m_mipmap ) bool ret = m_texture.generateMipmap();
 		m_texture.setSmooth( m_smooth );
 	}
 
@@ -558,7 +556,7 @@ bool FeTextureContainer::tick( FeSettings *feSettings, bool play_movies )
 		if ( il.check_loaded( m_entry ) )
 		{
 			m_texture.update( m_entry->get_data() );
-			if ( m_mipmap ) m_texture.generateMipmap();
+			if ( m_mipmap ) bool ret = m_texture.generateMipmap();
 			m_texture.setSmooth( m_smooth );
 
 			il.release_entry( &m_entry );
@@ -609,7 +607,7 @@ bool FeTextureContainer::tick( FeSettings *feSettings, bool play_movies )
 
 		if ( m_movie->tick() )
 		{
-			if ( m_mipmap ) m_texture.generateMipmap();
+			if ( m_mipmap ) bool ret = m_texture.generateMipmap();
 			return true;
 		}
 	}
@@ -846,7 +844,7 @@ bool FeTextureContainer::get_smooth() const
 void FeTextureContainer::set_mipmap( bool m )
 {
 	m_mipmap = m;
-	if ( m_mipmap && !m_movie) m_texture.generateMipmap();
+	if ( m_mipmap && !m_movie ) bool ret = m_texture.generateMipmap();
 }
 
 bool FeTextureContainer::get_mipmap() const
@@ -902,10 +900,10 @@ FeSurfaceTextureContainer::FeSurfaceTextureContainer( int width, int height )
 	if ( fep )
 	{
 		FeSettings *fes = fep->get_fes();
-		if ( fes ) ctx.antialiasingLevel = fes->get_antialiasing();
+		if ( fes ) ctx.antiAliasingLevel = fes->get_antialiasing();
 	}
-	m_texture.create( width, height, ctx );
-	m_texture.clear( sf::Color::Transparent );
+	if ( m_texture.resize({ static_cast<unsigned int>(width), static_cast<unsigned int>(height) }, ctx ) )
+		m_texture.clear( sf::Color::Transparent );
 }
 
 FeSurfaceTextureContainer::~FeSurfaceTextureContainer()
@@ -961,7 +959,7 @@ void FeSurfaceTextureContainer::on_redraw_surfaces()
 		}
 
 		m_texture.display();
-		if ( m_mipmap ) m_texture.generateMipmap();
+		if ( m_mipmap ) bool ret = m_texture.generateMipmap();
 	}
 }
 
@@ -1089,7 +1087,7 @@ void FeImage::texture_changed( FeBaseTextureContainer *new_tex )
 
 	//  reset texture rect now to the one reported by the new texture object
 	m_sprite.setTextureRect(
-		sf::FloatRect( 0, 0, m_tex->get_texture().getSize().x, m_tex->get_texture().getSize().y ) );
+		sf::FloatRect({ 0, 0 }, { static_cast<float>( m_tex->get_texture().getSize().x ), static_cast<float>( m_tex->get_texture().getSize().y )}));
 
 	scale();
 }
@@ -1187,7 +1185,7 @@ void FeImage::scale()
 	sf::FloatRect texture_rect = m_sprite.getTextureRect();
 	float ratio = m_tex->get_sample_aspect_ratio();
 
-	if (( texture_rect.width == 0 ) || ( texture_rect.height == 0 ))
+	if (( texture_rect.size.x == 0 ) || ( texture_rect.size.y == 0 ))
 		return;
 
 	bool scale=false;
@@ -1196,7 +1194,7 @@ void FeImage::scale()
 
 	if ( m_size.x > 0.0 )
 	{
-		scale_x = (float) m_size.x / abs( texture_rect.width );
+		scale_x = (float) m_size.x / abs( texture_rect.size.x );
 
 		if ( m_preserve_aspect_ratio )
 			scale_y = scale_x;
@@ -1206,7 +1204,7 @@ void FeImage::scale()
 
 	if ( m_size.y > 0.0 )
 	{
-		scale_y = (float) m_size.y / abs( texture_rect.height );
+		scale_y = (float) m_size.y / abs( texture_rect.size.y );
 
 		if ( m_preserve_aspect_ratio )
 		{
@@ -1220,15 +1218,12 @@ void FeImage::scale()
 				// ratio, so calculate how we will centre the image in the space we have
 				//
 				sf::Transform t;
-				t.rotate( m_rotation );
+				t.rotate( sf::degrees( m_rotation ));
 
 				if ( scale_x > scale_y * ratio ) // centre in x direction
-					final_pos += t.transformPoint(
-						( m_size.x - abs( texture_rect.width ) * scale_y * ratio ) / 2.0,
-						0 );
+					final_pos += t.transformPoint({ static_cast<float>(( m_size.x - abs( texture_rect.size.x ) * scale_y * ratio ) / 2.0 ), 0 });
 				else // centre in y direction
-					final_pos += t.transformPoint( 0,
-						( m_size.y - abs( texture_rect.height ) * scale_x / ratio ) / 2.0 );
+					final_pos += t.transformPoint({ 0, static_cast<float>(( m_size.y - abs( texture_rect.size.y ) * scale_x / ratio ) / 2.0 )});
 			}
 		}
 
@@ -1249,16 +1244,16 @@ void FeImage::scale()
 	final_pos += sf::Vector2f(( m_rotation_origin.x - m_anchor.x ) * m_size.x, ( m_rotation_origin.y -  m_anchor.y ) * m_size.y );
 
 	m_sprite.setPosition( final_pos );
-	m_sprite.setRotation( m_rotation );
-	m_sprite.setOrigin(( m_origin.x + m_rotation_origin.x * m_size.x ) / scale_x, ( m_origin.y + m_rotation_origin.y * m_size.y ) / scale_y );
+	m_sprite.setRotation( sf::degrees( m_rotation ));
+	m_sprite.setOrigin({( m_origin.x + m_rotation_origin.x * m_size.x ) / scale_x, ( m_origin.y + m_rotation_origin.y * m_size.y ) / scale_y });
 }
 
-const sf::Vector2f &FeImage::getPosition() const
+sf::Vector2f FeImage::getPosition() const
 {
 	return m_pos;
 }
 
-const sf::Vector2f &FeImage::getSize() const
+sf::Vector2f FeImage::getSize() const
 {
 	return m_size;
 }
@@ -1298,12 +1293,12 @@ void FeImage::setRotation( float r )
 	}
 }
 
-const sf::Color &FeImage::getColor() const
+sf::Color FeImage::getColor() const
 {
 	return m_sprite.getColor();
 }
 
-void FeImage::setColor( const sf::Color &c )
+void FeImage::setColor( sf::Color c )
 {
 	if ( c != m_sprite.getColor() )
 	{
@@ -1312,12 +1307,12 @@ void FeImage::setColor( const sf::Color &c )
 	}
 }
 
-const sf::Vector2u FeImage::getTextureSize() const
+sf::Vector2u FeImage::getTextureSize() const
 {
 	return m_tex->get_texture().getSize();
 }
 
-const sf::FloatRect &FeImage::getTextureRect() const
+sf::FloatRect FeImage::getTextureRect() const
 {
 	return m_sprite.getTextureRect();
 }
@@ -1603,22 +1598,22 @@ int FeImage::get_texture_height() const
 
 float FeImage::get_subimg_x() const
 {
-	return getTextureRect().left;
+	return getTextureRect().position.x;
 }
 
 float FeImage::get_subimg_y() const
 {
-	return getTextureRect().top;
+	return getTextureRect().position.y;
 }
 
 float FeImage::get_subimg_width() const
 {
-	return getTextureRect().width;
+	return getTextureRect().size.x;
 }
 
 float FeImage::get_subimg_height() const
 {
-	return getTextureRect().height;
+	return getTextureRect().size.y;
 }
 
 float FeImage::get_sample_aspect_ratio() const
@@ -1634,28 +1629,28 @@ bool FeImage::get_preserve_aspect_ratio() const
 void FeImage::set_subimg_x( float x )
 {
 	sf::FloatRect r = getTextureRect();
-	r.left=x;
+	r.position.x=x;
 	setTextureRect( r );
 }
 
 void FeImage::set_subimg_y( float y )
 {
 	sf::FloatRect r = getTextureRect();
-	r.top=y;
+	r.position.y=y;
 	setTextureRect( r );
 }
 
 void FeImage::set_subimg_width( float w )
 {
 	sf::FloatRect r = getTextureRect();
-	r.width=w;
+	r.size.x=w;
 	setTextureRect( r );
 }
 
 void FeImage::set_subimg_height( float h )
 {
 	sf::FloatRect r = getTextureRect();
-	r.height=h;
+	r.size.y=h;
 	setTextureRect( r );
 }
 

src/fe_image.hpp
@@ -287,22 +287,22 @@ public:
 
 	const sf::Texture *get_texture();
 
-	const sf::Vector2f &getSize() const;
+	sf::Vector2f getSize() const;
 	void setSize( const sf::Vector2f &s );
 	void setSize( int w, int h ) { setSize( sf::Vector2f( w, h ) ); };
-	const sf::Vector2f &getPosition() const;
+	sf::Vector2f getPosition() const;
 	void setPosition( const sf::Vector2f & );
 	void setPosition( int x, int y ) { setPosition( sf::Vector2f( x, y ));};
 	float getRotation() const;
 	void setRotation( float );
-	const sf::Color &getColor() const;
-	void setColor( const sf::Color & );
+	sf::Color getColor() const;
+	void setColor( sf::Color );
 	int getIndexOffset() const;
 	void setIndexOffset(int);
 	int getFilterOffset() const;
 	void setFilterOffset(int);
-	const sf::Vector2u getTextureSize() const;
-	const sf::FloatRect &getTextureRect() const;
+	sf::Vector2u getTextureSize() const;
+	sf::FloatRect getTextureRect() const;
 	void setTextureRect( const sf::FloatRect &);
 	int getVideoFlags() const;
 	void setVideoFlags( int f );

commit b9b6edcf0c9caaf877915557def0d1ac21ee8dbf

BP: Fix infinite layout reload on resizing the window
Regression caused by commit: b8f1ba73a

src/fe_vm.cpp
@@ -412,6 +412,7 @@ void FeVM::post_command( FeInputMap::Command c )
 bool FeVM::poll_command( FeInputMap::Command &c, std::optional<FeEvent> &ev, bool &from_ui )
 {
 	from_ui = false;
+	ev = std::nullopt;
 
 	if ( !m_posted_commands.empty() )
 	{
@@ -441,7 +442,6 @@ bool FeVM::poll_command( FeInputMap::Command &c, std::optional<FeEvent> &ev, boo
 		}
 	}
 
-	ev = std::nullopt; // An empty event
 	return false;
 }
 