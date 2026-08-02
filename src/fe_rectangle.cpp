oomek/attractplus

commit 202f964df4a0e587ff854f8d7b6cc5a99f8e6907

SDL Fixes
- blend modes
- rectangle outline
- image/surface clone properties

src/fe_rectangle.cpp
@@ -605,9 +605,7 @@ bool FeRectangle::build_render_geometry( FeRenderGeometry &geometry ) const
 		return false;
 
 	const sf::Transform transform = m_rect.getTransform();
-	const sf::Color color = m_rect.getFillColor();
-	const sf::Vector2f first = transform.transformPoint( m_rect.getPoint( 0 ) );
-	const sf::Vector2f first_local = m_rect.getPoint( 0 );
+	const sf::Color fill_color = m_rect.getFillColor();
 	const sf::Vector2f rect_size = m_rect.getSize();
 
 	auto normalized_uv = [&]( const sf::Vector2f &point )
@@ -616,6 +614,44 @@ bool FeRectangle::build_render_geometry( FeRenderGeometry &geometry ) const
 		const float v = ( rect_size.y != 0.0f ) ? ( point.y / rect_size.y ) : 0.0f;
 		return sf::Vector2f( u, v );
 	};
+	auto append_triangle = [&]( const sf::Vector2f &p0,
+		const sf::Vector2f &p1,
+		const sf::Vector2f &p2,
+		const sf::Vector2f &uv0,
+		const sf::Vector2f &uv1,
+		const sf::Vector2f &uv2,
+		const sf::Color &color )
+	{
+		FeRenderVertex v0 = {};
+		v0.x = p0.x;
+		v0.y = p0.y;
+		v0.z = get_z();
+		v0.u = uv0.x;
+		v0.v = uv0.y;
+		v0.r = color.r;
+		v0.g = color.g;
+		v0.b = color.b;
+		v0.a = color.a;
+
+		FeRenderVertex v1 = v0;
+		v1.x = p1.x;
+		v1.y = p1.y;
+		v1.u = uv1.x;
+		v1.v = uv1.y;
+
+		FeRenderVertex v2 = v0;
+		v2.x = p2.x;
+		v2.y = p2.y;
+		v2.u = uv2.x;
+		v2.v = uv2.y;
+
+		geometry.vertices.push_back( v0 );
+		geometry.vertices.push_back( v1 );
+		geometry.vertices.push_back( v2 );
+	};
+
+	const sf::Vector2f first_local = m_rect.getPoint( 0 );
+	const sf::Vector2f first = transform.transformPoint( first_local );
 
 	for ( std::size_t i = 1; i + 1 < point_count; ++i )
 	{
@@ -627,32 +663,52 @@ bool FeRectangle::build_render_geometry( FeRenderGeometry &geometry ) const
 		const sf::Vector2f uv1 = normalized_uv( second_local );
 		const sf::Vector2f uv2 = normalized_uv( third_local );
 
-		FeRenderVertex v0 = {};
-		v0.x = first.x;
-		v0.y = first.y;
-		v0.z = get_z();
-		v0.u = uv0.x;
-		v0.v = uv0.y;
-		v0.r = color.r;
-		v0.g = color.g;
-		v0.b = color.b;
-		v0.a = color.a;
+		append_triangle( first, second, third, uv0, uv1, uv2, fill_color );
+	}
 
-		FeRenderVertex v1 = v0;
-		v1.x = second.x;
-		v1.y = second.y;
-		v1.u = uv1.x;
-		v1.v = uv1.y;
+	const float outline = m_rect.getOutlineThickness();
+	const sf::Color outline_color = m_rect.getOutlineColor();
+	if ( outline > 0.0f && outline_color.a > 0 )
+	{
+		const unsigned int corner_points = static_cast<unsigned int>( point_count / 4 );
+		sf::RoundedRectangleShape outer_rect(
+			sf::Vector2f( rect_size.x + outline * 2.0f, rect_size.y + outline * 2.0f ),
+			sf::Vector2f(
+				std::max( 0.0f, m_rect.getCornerRadius().x + outline ),
+				std::max( 0.0f, m_rect.getCornerRadius().y + outline ) ),
+			corner_points );
+		const sf::Vector2f outline_offset( outline, outline );
 
-		FeRenderVertex v2 = v0;
-		v2.x = third.x;
-		v2.y = third.y;
-		v2.u = uv2.x;
-		v2.v = uv2.y;
+		for ( std::size_t i = 0; i < point_count; ++i )
+		{
+			const std::size_t next = ( i + 1 ) % point_count;
+			const sf::Vector2f inner_local_0 = m_rect.getPoint( i );
+			const sf::Vector2f inner_local_1 = m_rect.getPoint( next );
+			const sf::Vector2f outer_local_0 = outer_rect.getPoint( i ) - outline_offset;
+			const sf::Vector2f outer_local_1 = outer_rect.getPoint( next ) - outline_offset;
 
-		geometry.vertices.push_back( v0 );
-		geometry.vertices.push_back( v1 );
-		geometry.vertices.push_back( v2 );
+			const sf::Vector2f inner_0 = transform.transformPoint( inner_local_0 );
+			const sf::Vector2f inner_1 = transform.transformPoint( inner_local_1 );
+			const sf::Vector2f outer_0 = transform.transformPoint( outer_local_0 );
+			const sf::Vector2f outer_1 = transform.transformPoint( outer_local_1 );
+
+			append_triangle(
+				outer_0,
+				outer_1,
+				inner_0,
+				normalized_uv( outer_local_0 ),
+				normalized_uv( outer_local_1 ),
+				normalized_uv( inner_local_0 ),
+				outline_color );
+			append_triangle(
+				inner_0,
+				outer_1,
+				inner_1,
+				normalized_uv( inner_local_0 ),
+				normalized_uv( outer_local_1 ),
+				normalized_uv( inner_local_1 ),
+				outline_color );
+		}
 	}
 
 	geometry.texture_id = nullptr;

src/fe_blend.cpp
@@ -22,6 +22,7 @@
 
 #include "fe_blend.hpp"
 #include "fe_util.hpp" // for FE_VERSION_INT macro
+#include "fe_base.hpp"
 
 namespace
 {
@@ -52,6 +53,7 @@ namespace
 		"gl_FragColor = gl_Color * pixel;" \
 		"gl_FragColor.xyz *= gl_Color.w;}";
 
+	sf::Shader *default_shader_alpha=NULL;
 	sf::Shader *default_shader_multiplied=NULL;
 	sf::Shader *default_shader_overlay=NULL;
 	sf::Shader *default_shader_premultiplied=NULL;
@@ -88,15 +90,25 @@ sf::BlendMode FeBlend::get_blend_mode( int blend_mode )
 	}
 }
 
+bool FeBlend::uses_default_shader( int blend_mode )
+{
+	return ( get_default_shader_source( blend_mode ) != NULL );
+}
+
 sf::Shader* FeBlend::get_default_shader( int blend_mode )
 {
+	if ( !uses_default_shader( blend_mode ) )
+		return NULL;
+
 	switch( blend_mode )
 	{
 		case FeBlend::Alpha:
-		case FeBlend::Add:
-		case FeBlend::Subtract:
-		case FeBlend::None:
-			return NULL;
+			if ( !default_shader_alpha )
+			{
+				default_shader_alpha = new sf::Shader();
+				std::ignore = default_shader_alpha->loadFromMemory( DEFAULT_SHADER_GLSL_ALPHA, sf::Shader::Type::Fragment );
+			}
+			return default_shader_alpha;
 		case FeBlend::Screen:
 		case FeBlend::Multiply:
 			if ( !default_shader_multiplied )
@@ -129,21 +141,14 @@ const char *FeBlend::get_default_shader_source( int blend_mode )
 	switch( blend_mode )
 	{
 		case FeBlend::Alpha:
-		case FeBlend::Add:
-		case FeBlend::Subtract:
-		case FeBlend::None:
 			return DEFAULT_SHADER_GLSL_ALPHA;
-
 		case FeBlend::Screen:
 		case FeBlend::Multiply:
 			return DEFAULT_SHADER_GLSL_MULTIPLIED;
-
 		case FeBlend::Overlay:
 			return DEFAULT_SHADER_GLSL_OVERLAY;
-
 		case FeBlend::Premultiplied:
 			return DEFAULT_SHADER_GLSL_PREMULTIPLIED;
-
 		default:
 			return NULL;
 	}
@@ -151,6 +156,12 @@ const char *FeBlend::get_default_shader_source( int blend_mode )
 
 void FeBlend::clear_default_shaders()
 {
+	if ( default_shader_alpha )
+	{
+		delete default_shader_alpha;
+		default_shader_alpha = NULL;
+	}
+
 	if ( default_shader_multiplied )
 	{
 		delete default_shader_multiplied;

src/fe_blend.hpp
@@ -41,6 +41,7 @@ public:
 	};
 
 	static sf::BlendMode get_blend_mode( int blend_mode );
+	static bool uses_default_shader( int blend_mode );
 	static sf::Shader* get_default_shader( int blend_mode );
 	static const char *get_default_shader_source( int blend_mode );
 

src/fe_image.cpp
@@ -1528,15 +1528,14 @@ FeImage::FeImage(
 	scale();
 }
 
-FeImage::FeImage( FeImage *o ):
-	FeBasePresentable( *o ),
+FeImage::FeImage( FeImage *o, FePresentableParent &p ):
+	FeBasePresentable( p ),
 	m_tex( o->m_tex ),
-	m_sprite( o->m_sprite ),
 	m_pos( o->m_pos ),
 	m_size( o->m_size ),
 	m_auto_size( o->m_auto_size ),
 	m_origin( o->m_origin ),
-	m_transform_origin( o->m_anchor ),
+	m_transform_origin( o->m_transform_origin ),
 	m_transform_origin_type( o->m_transform_origin_type ),
 	m_anchor( o->m_anchor ),
 	m_anchor_type( o->m_anchor_type ),
@@ -1564,8 +1563,15 @@ FeImage::FeImage( FeImage *o ):
 	m_fft_zero_wrapper( &m_fft_data_zero ),
 	m_fft_array_wrapper( &m_fft_data_zero )
 {
+	set_visible( o->get_visible() );
+	set_z( o->get_z() );
+	script_set_shader( o->get_shader() );
+	set_rotation_x( o->get_rotation_x() );
+	set_rotation_y( o->get_rotation_y() );
+	set_zorder( o->get_zorder() );
 	set_smooth( o->get_smooth() );
 	m_tex->register_image( this );
+	texture_changed();
 }
 
 FeImage::~FeImage() {}

src/fe_image.hpp
@@ -358,7 +358,7 @@ public:
 		float h
 	);
 
-	FeImage( FeImage * ); // clone the given image (texture is not copied)
+	FeImage( FeImage *, FePresentableParent & ); // clone the given image (texture is not copied)
 	~FeImage();
 
 	const sf::Texture *get_texture() const;

src/fe_present.cpp
@@ -192,7 +192,8 @@ FePresent::FePresent( FeSettings *fesettings, FeWindow &wnd )
 	m_overlay_caption( NULL ),
 	m_overlay_lb( NULL ),
 	m_layout_loaded( false ),
-	m_layout_has_content( false )
+	m_layout_has_content( false ),
+	m_render_frame_serial( 0 )
 {
 	m_baseRotation = m_feSettings->get_screen_rotation();
 	m_layoutFontName = "";
@@ -448,6 +449,8 @@ void FePresent::clear_resources()
 //
 void FePresent::clear_layout()
 {
+	m_window.get_gpu_context().clear_layout_resources();
+
 	//
 	// keep toggle rotation, base rotation and mute state through clear
 	//
@@ -639,6 +642,13 @@ void FePresent::build_render_surface_frames( std::vector<FeRenderSurfaceFrame> &
 		std::uint64_t signature;
 		bool dynamic_content;
 	};
+	struct PendingSurfaceFrame
+	{
+		FeSurfaceTextureContainer *surface;
+		FeRenderSurfaceFrame frame;
+		std::vector<const FeSurfaceTextureContainer *> dependencies;
+		std::size_t original_index;
+	};
 
 	auto hash_combine = []( std::uint64_t seed, std::uint64_t value ) -> std::uint64_t
 	{
@@ -684,11 +694,11 @@ void FePresent::build_render_surface_frames( std::vector<FeRenderSurfaceFrame> &
 			return a->get_nesting_level() > b->get_nesting_level();
 		} );
 
-	std::unordered_map<const FeSurfaceTextureContainer *, SurfaceState> surface_states;
-	surface_states.reserve( surface_list.size() );
-
-	for ( FeSurfaceTextureContainer *surface : surface_list )
+	std::vector<PendingSurfaceFrame> pending_frames;
+	pending_frames.reserve( surface_list.size() );
+	for ( std::size_t surface_index = 0; surface_index < surface_list.size(); ++surface_index )
 	{
+		FeSurfaceTextureContainer *surface = surface_list[ surface_index ];
 		FeRenderSurfaceFrame frame;
 		frame.surface_id = surface;
 		frame.surface_texture_id = surface;
@@ -703,6 +713,7 @@ void FePresent::build_render_surface_frames( std::vector<FeRenderSurfaceFrame> &
 		frame.camera.update_projection( static_cast<float>( frame.width ), static_cast<float>( frame.height ) );
 		frame.geometry_signature = 1469598103934665603ULL;
 		frame.content_signature = 1469598103934665603ULL;
+		std::vector<const FeSurfaceTextureContainer *> dependencies;
 
 		for ( const FeBasePresentable *presentable : surface->elements )
 		{
@@ -715,24 +726,16 @@ void FePresent::build_render_surface_frames( std::vector<FeRenderSurfaceFrame> &
 				FeRenderGeometry image_geometry;
 				if ( image->build_render_geometry( image_geometry ) )
 				{
-					bool referenced_surface_dynamic = false;
 					if ( image_geometry.texture_source_type == FeRenderTextureSourceContainer )
 					{
 						const FeSurfaceTextureContainer *referenced_surface =
 							dynamic_cast<const FeSurfaceTextureContainer *>(
 								static_cast<const FeBaseTextureContainer *>( image_geometry.texture_id ) );
-						if ( referenced_surface )
-						{
-							auto child_it = surface_states.find( referenced_surface );
-							if ( child_it != surface_states.end() )
-							{
-								frame.content_signature = hash_combine( frame.content_signature, child_it->second.signature );
-								referenced_surface_dynamic = child_it->second.dynamic_content;
-							}
-						}
+						if ( referenced_surface && referenced_surface != surface )
+							dependencies.push_back( referenced_surface );
 					}
 
-					frame.dynamic_content = frame.dynamic_content || image_geometry.texture_dynamic || image_geometry.custom_shader || referenced_surface_dynamic;
+					frame.dynamic_content = frame.dynamic_content || image_geometry.texture_dynamic || image_geometry.custom_shader;
 					frame.geometry_signature = hash_geometry( image_geometry, frame.geometry_signature );
 					frame.content_signature = hash_geometry( image_geometry, frame.content_signature );
 					frame.geometry.push_back( image_geometry );
@@ -787,8 +790,86 @@ void FePresent::build_render_surface_frames( std::vector<FeRenderSurfaceFrame> &
 		if ( !frame.clear && frame.redraw )
 			frame.dynamic_content = true;
 
-		surface_states[ surface ] = { frame.content_signature, frame.dynamic_content };
-		surfaces.push_back( frame );
+		std::sort( dependencies.begin(), dependencies.end() );
+		dependencies.erase( std::unique( dependencies.begin(), dependencies.end() ), dependencies.end() );
+		pending_frames.push_back( { surface, frame, dependencies, surface_index } );
+	}
+
+	std::unordered_map<const FeSurfaceTextureContainer *, std::size_t> pending_indices;
+	pending_indices.reserve( pending_frames.size() );
+	for ( std::size_t i = 0; i < pending_frames.size(); ++i )
+		pending_indices[ pending_frames[i].surface ] = i;
+
+	std::vector<int> indegree( pending_frames.size(), 0 );
+	std::vector<std::vector<std::size_t>> dependents( pending_frames.size() );
+	for ( std::size_t i = 0; i < pending_frames.size(); ++i )
+	{
+		for ( const FeSurfaceTextureContainer *dependency : pending_frames[i].dependencies )
+		{
+			auto it = pending_indices.find( dependency );
+			if ( it == pending_indices.end() )
+				continue;
+
+			++indegree[i];
+			dependents[it->second].push_back( i );
+		}
+	}
+
+	std::vector<std::size_t> ordered_indices;
+	ordered_indices.reserve( pending_frames.size() );
+	std::vector<bool> emitted( pending_frames.size(), false );
+	while ( ordered_indices.size() < pending_frames.size() )
+	{
+		std::size_t chosen = pending_frames.size();
+		for ( std::size_t i = 0; i < pending_frames.size(); ++i )
+		{
+			if ( emitted[i] || indegree[i] != 0 )
+				continue;
+
+			if ( chosen == pending_frames.size() ||
+				pending_frames[i].frame.nesting_level > pending_frames[chosen].frame.nesting_level ||
+				( pending_frames[i].frame.nesting_level == pending_frames[chosen].frame.nesting_level &&
+					pending_frames[i].original_index < pending_frames[chosen].original_index ) )
+			{
+				chosen = i;
+			}
+		}
+
+		if ( chosen == pending_frames.size() )
+		{
+			for ( std::size_t i = 0; i < pending_frames.size(); ++i )
+			{
+				if ( emitted[i] )
+					continue;
+
+				chosen = i;
+				break;
+			}
+		}
+
+		emitted[chosen] = true;
+		ordered_indices.push_back( chosen );
+		for ( std::size_t dependent : dependents[chosen] )
+			--indegree[dependent];
+	}
+
+	std::unordered_map<const FeSurfaceTextureContainer *, SurfaceState> surface_states;
+	surface_states.reserve( pending_frames.size() );
+	for ( std::size_t ordered_index : ordered_indices )
+	{
+		PendingSurfaceFrame &pending = pending_frames[ordered_index];
+		for ( const FeSurfaceTextureContainer *dependency : pending.dependencies )
+		{
+			auto it = surface_states.find( dependency );
+			if ( it == surface_states.end() )
+				continue;
+
+			pending.frame.content_signature = hash_combine( pending.frame.content_signature, it->second.signature );
+			pending.frame.dynamic_content = pending.frame.dynamic_content || it->second.dynamic_content;
+		}
+
+		surface_states[ pending.surface ] = { pending.frame.content_signature, pending.frame.dynamic_content };
+		surfaces.push_back( pending.frame );
 	}
 }
 
@@ -824,7 +905,7 @@ FeImage *FePresent::add_image( bool is_artwork,
 FeImage *FePresent::add_clone( FeImage *o,
 			FePresentableParent &p )
 {
-	FeImage *new_image = new FeImage( o );
+	FeImage *new_image = new FeImage( o, p );
 	flag_redraw();
 	p.elements.push_back( new_image );
 
@@ -1653,7 +1734,7 @@ void FePresent::submit_render_frame()
 	frame.camera = m_layout_camera;
 	frame.viewport_width = m_mon[0].size.x;
 	frame.viewport_height = m_mon[0].size.y;
-	frame.frame_number = static_cast<unsigned long long>( m_layout_time.getElapsedTime().asMilliseconds() );
+	frame.frame_number = ++m_render_frame_serial;
 	build_render_geometry( frame.images );
 	build_render_surface_frames( frame.surfaces );
 	frame.image_count = static_cast<unsigned long long>( frame.images.size() );

src/fe_present.hpp
@@ -180,6 +180,7 @@ protected:
 	FeListBox *m_overlay_lb;
 	bool m_layout_loaded;
 	bool m_layout_has_content;
+	unsigned long long m_render_frame_serial;
 
 	FePresent( const FePresent & );
 	FePresent &operator=( const FePresent & );

src/fe_renderer.cpp
@@ -119,9 +119,9 @@ void FeRenderSurfaceFrame::clear_frame()
 
 FeRenderFrame::FeRenderFrame()
 	: viewport_width( 0 ),
-	viewport_height( 0 ),
-	frame_number( 0 ),
-	image_count( 0 )
+	  viewport_height( 0 ),
+	  frame_number( 0 ),
+	  image_count( 0 )
 {
 }
 

commit 2727e7a9351db933dfb625b35212c341d12ce3bd

Fix FeRectangle negative outline not showing
- Regression caused by 202f964df4a0e587ff854f8d7b6cc5a99f8e6907

src/fe_rectangle.cpp
@@ -752,7 +752,7 @@ bool FeRectangle::build_render_geometry( FeRenderGeometry &geometry ) const
 
 	const float outline = m_outline_thickness;
 	const Color outline_color = m_outline_color;
-	if ( outline > 0.0f && outline_color.a > 0 )
+	if ( outline != 0.0f && outline_color.a > 0 )
 	{
 		const unsigned int corner_points = static_cast<unsigned int>( m_corner_point_actual );
 		const Vec2f outer_size( rect_size.x + outline * 2.0f, rect_size.y + outline * 2.0f );